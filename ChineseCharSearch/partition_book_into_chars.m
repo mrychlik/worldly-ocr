0150a419aa6fddc5dfc86ae0c11c20f890612551
Marek Rychlik <marek@marekrychlik.com>
2019-01-19 04:06:47 -0700

    *** empty log message ***

--- ChineseCharSearch/partition_book_into_chars.m
+++ ChineseCharSearch/partition_book_into_chars.m
@@ -1,8 +1,7 @@
 function char_seq = partition_book_into_chars(book_strip_image)
-%  CHAR_SEQ = PARTITION_BOOK_INTO_CHARS(BOOK_STRIP_IMAGE)
-% takes the image BOOK_STRIP_IMAGE which contains stacked
-% columns of all pages of a book, and it outputs the sequence of characters
-% contained in the image.
+% CHAR_SEQ = PARTITION_BOOK_INTO_CHARS(BOOK_STRIP_IMAGE) takes the image
+% BOOK_STRIP_IMAGE which contains stacked columns of all pages of a book,
+% and it outputs the sequence of characters contained in the image.
 %
 % As this computation is rather expensive, we cache the result in directory
 % Cache, file CharSeq.mat. The user must wipe out this file manually if the
