41f6e5844bab05d8285dc706be3f8a3f615d78b2
Marek Rychlik <marek@marekrychlik.com>
2019-01-19 04:13:17 -0700

    *** empty log message ***

--- ChineseCharSearch/partition_book_into_chars.m
+++ ChineseCharSearch/partition_book_into_chars.m
@@ -8,7 +8,7 @@ function char_seq = partition_book_into_chars(book_strip_image)
 % Cache, file CharSeq.mat. The user must wipe out this file manually if the
 % input file BOOK_STRIP_IMAGE changes.
 %
-nargchk(nargin, 1);
+nargchk(nargin, 1, 1);
 
 Display='off';
 
