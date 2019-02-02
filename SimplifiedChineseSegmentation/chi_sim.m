d41e49388c09893fbdedc4ce63eb6db1ff09ac0b
Marek Rychlik <marek@marekrychlik.com>
2019-02-02 14:33:36 -0700

    *** empty log message ***

--- SimplifiedChineseSegmentation/chi_sim.m
+++ SimplifiedChineseSegmentation/chi_sim.m
@@ -10,9 +10,9 @@
 imagedir=fullfile('.','images');
 cachedir=fullfile('.','Cache');
 
-imagefile='Img_0322.jpg';
+% imagefile='Img_0322.jpg';
 % imagefile='02348.jpg';
-% imagefile='02351.jpg';
+imagefile='02351.jpg';
 
 
 filepath=fullfile(imagedir,imagefile);
