3c32f6f6795f2053e9e061d795551a03fc5e1665
Marek Rychlik <marek@marekrychlik.com>
2019-02-04 15:45:00 -0700

    *** empty log message ***

---
+++ SimplifiedChineseSegmentation/play_chars.m
@@ -0,0 +1,39 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% This script preloads/computes the lines of text
+%%%
+%%% This file is a preamble to other files, which perform real processing
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Where to find pages of scanned text
+imagedir=fullfile('.','images');
+
+% imagefile='Img_0322.jpg';
+% imagefile='02348.jpg';
+imagefile='02351.jpg';
+
+padding = 20;
+
+filepath=fullfile(imagedir,imagefile);
+
+
+
+%% Build a cache of objects in the image
+% Strings identifying page numbers in image files
+% Where to save objects
+objects=[];
+I=imread(filepath);                 % Note: the image is BW
+BW=im2bw(I,.7);
+BW=~BW;
+conn=4;
+[L,NUM] = bwlabel(BW,conn);
+disp(sprintf('Found %d objects',NUM));
+for n=0:NUM
+    ob = L==n;
+    ob_cropped = imautocrop(ob, padding);
+    imagesc(ob_cropped);
+    drawnow;
+    pause(.1);
+end
+
