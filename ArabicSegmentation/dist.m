38477788194332cdc399ad40172eb388724f53df
Marek Rychlik <marek@marekrychlik.com>
2019-02-07 16:31:23 -0700

    *** empty log message ***

--- ArabicSegmentation/dist.m
+++ ArabicSegmentation/dist.m
@@ -1,4 +1,20 @@
 function d = dist(ob1,ob2)
-A = pdist2(ob1.PixelList, ob2.PixelList);
+A = pdist2(ob1.BoundingBox, ob2.BoundingBox, @bbox_dist);
 d = min(A(:));
+end
+
+function d = bbox_dist(b1, b2)
+    d1 = ival_dist([b1(1),b1(1)+b1(3)],[b2(1),b2(1)+b2(3)]);
+    d2 = ival_dist([b1(2),b1(2)+b1(4)],[b2(2),b2(2)+b2(4)]);
+    d = min(d1, d2);
+end    
+
+function d = ival_dist(i1, i2)
+    if i1(1) > i2(2)
+        d = i2(2) - i1(1)
+    elseif i1(2) < i2(1)
+        d = i2(1) - i1(2)
+    else
+        d = 0
+    end
 end
