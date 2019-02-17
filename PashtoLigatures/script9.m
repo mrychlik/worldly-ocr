e45b41ac31d2f0d8af7622013db4b8b0333f0106
Marek Rychlik <marek@marekrychlik.com>
2019-02-16 19:02:55 -0700

    *** empty log message ***

--- PashtoLigatures/script9.m
+++ PashtoLigatures/script9.m
@@ -48,11 +48,11 @@ for x = (M-win):-1:0
     K(:,(x+1):(x+win),3) = 0;        
 
 
-    subplot(2,3,1), imshow(K0), colormap(gray);
-    subplot(2,3,4), imshow(K), colormap(gray);
+    subplot(2,3,1), imagesc(K0), colormap(gray);
+    subplot(2,3,4), imagesc(K), colormap(gray);
 
-    subplot(2,3,2), imshow(col0);
-    subplot(2,3,5), imshow(col);
+    subplot(2,3,2), imagesc(col0);
+    subplot(2,3,5), imagesc(col);
 
     L0 = fft2(col0);
     L = fft2(col);
