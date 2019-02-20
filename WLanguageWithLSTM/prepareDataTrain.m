b8affc85f51bf28d68be024edc107cb08c54072c
Marek Rychlik <marek@marekrychlik.com>
2019-02-20 11:52:23 -0700

    *** empty log message ***

--- WLanguageWithLSTM/prepareDataTrain.m
+++ WLanguageWithLSTM/prepareDataTrain.m
@@ -1,7 +1,18 @@
 function [XTrain, YTrain] = prepareDataTrain(num_samples,sample_length, max_stretch)
 % Create a trainint dataset in W-language
-%  [XTRAIN, YTRAIN] = PREPAREDATATRAIN outputs:
-%     XTRAIN - a cell array of L-by-3 matrices
+%  [XTRAIN, YTRAIN] = PREPAREDATATRAIN(NUM_SAMPLES,SAMPLE_LENGTH, MAX_STRETCH) outputs:
+%     XTRAIN - a cell array of NUM_SAMPLES SAMPLE_LENGTH-by-3 matrices of 0-1;
+%     YTRAIN - a cell array of NUM_SAMPLES strings of length
+%              SAMPLE_LENGTH, with characters in the set {'X','O','_'}
+%
+%  The YTRAIN provides the responses to patterns in XTRAIN, which are the 
+%  decoded strings of the W-language, emitted as per the algorithm
+%  described in the paper 
+%
+%      Deductron - A Recurrent Neural Network
+%
+% published at https://arxiv.org/abs/1806.09038. The underscore '_' means
+% that there was no emission at the corresponding time point.
 % 
 nargchk(0, 3)
 if nargin < 1; num_samples = 1000; end
