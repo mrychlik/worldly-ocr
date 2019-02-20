db5d8569878efaaf7b13531407f2101a81315930
Marek Rychlik <marek@marekrychlik.com>
2019-02-20 11:47:00 -0700

    *** empty log message ***

--- WLanguageWithLSTM/prepareDataTrain.m
+++ WLanguageWithLSTM/prepareDataTrain.m
@@ -1,9 +1,15 @@
-function [XTrain, YTrain] = prepareDataTrain
+function [XTrain, YTrain] = prepareDataTrain(num_samples,sample_length, max_stretch)
+% Create a trainint dataset in W-language
+%  [XTRAIN, YTRAIN] = PREPAREDATATRAIN outputs:
+%     XTRAIN - a cell array of L-by-3 matrices
+% 
+nargchk(0, 3)
+if nargin < 1; num_samples = 1000; end
+if nargin < 2; sample_length = 3;
+if nargin < 3; max_stretch = 1;
+    
+X = cell(num_samples, 1);
 
-N = 1000;
-L = 3;
-X = cell(N,1);
-max_stretch = 1;
 
 % Map random strings to W-language
 for j = 1:N
