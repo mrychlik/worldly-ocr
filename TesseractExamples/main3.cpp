6a9fb50d3178bde624fb92895845cfeec35b3dc6
Marek Rychlik <marek@marekrychlik.com>
2019-02-23 09:27:13 -0700

    *** empty log message ***

--- TesseractExamples/main3.cpp
+++ TesseractExamples/main3.cpp
@@ -14,36 +14,49 @@
 bool ocr(const char *const language, const char* const imagePath, const char *outPath)
 {
   printf("Doing %s\n", imagePath);
-  char *outText;
 
   tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-  // Initialize tesseract-ocr with English, without specifying tessdata path
-  if (api->Init(NULL, language)) {
-    fprintf(stderr, "Could not initialize tesseract.\n");
-    exit(1);
-  }
-
-  // Open input image with leptonica library
-  // Pix *image = pixRead("./images/Paragraph.tif");
-  Pix *image = pixRead(imagePath);
-
-  api->SetImage(image);
-  // Get OCR result
-  outText = api->GetUTF8Text();
-    
   FILE *outFile;
   bool status = true;
 
-  if((outFile = fopen(outPath,"w")) != NULL) {
-    fprintf(outFile, "OCR output for image %s:\n%s", imagePath, outText);
-    fclose(outFile);
-  } else {
-    status = false;
+
+  // Initialize tesseract-ocr with English, without specifying tessdata path
+  if (api->Init(NULL, language)) {
+    fprintf(stderr, "Could not initialize tesseract.\n");
+    return false;
   }
+
+  if((outFile = fopen(outPath,"w")) == NULL) {
+    fprintf(stderr, "Could not open output file.\n");
+    return false;
+  }
+
+  fprintf(outFile, "OCR output for image %s:\n", imagePath);
+
+  Pix *image = pixRead(imagePath);
+
+  api->Init(NULL, language);
+  api->SetImage(image);
+  api->Recognize(0);
+
+  tesseract::ResultIterator* ri = api->GetIterator();
+  tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
+  if (ri != 0) {
+    do {
+      const char* word = ri->GetUTF8Text(level);
+      float conf = ri->Confidence(level);
+      int x1, y1, x2, y2;
+      ri->BoundingBox(level, &x1, &y1, &x2, &y2);
+      fprintf(outFile, "word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
+	      word, conf, x1, y1, x2, y2);
+      delete[] word;
+    } while (ri->Next(level));
+  }
+
+  fclose(outFile);
 
   // Destroy used object and release memory
   api->End();
-  delete [] outText;
   pixDestroy(&image);
 
   return status;
