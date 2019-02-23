68a928274e59973620a925b58238b66b3bcbad82
Marek Rychlik <marek@marekrychlik.com>
2019-02-22 19:37:07 -0700

    *** empty log message ***

---
+++ TesseractExamples/main1.cpp
@@ -0,0 +1,32 @@
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+int main()
+{
+    char *outText;
+
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    // Pix *image = pixRead("/home/marek/Repos/Pashto/trunk/ReadingMicrofilm/images/Paragraph.tif");
+    Pix *image = pixRead("/home/marek/Repos/Pashto/trunk/Samples/Chinese/chinese-tra-testdata_1/chinese-tradition-0pic.png");
+
+
+
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+
+    // Destroy used object and release memory
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
+
+    return 0;
+}
