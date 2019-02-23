c619bfdb704a9593d4ba7d2ecccdf8ee1d65b195
Marek Rychlik <marek@marekrychlik.com>
2019-02-23 10:04:10 -0700

    *** empty log message ***

---
+++ TesseractExamples/main5.cpp
@@ -0,0 +1,75 @@
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+
+bool ocr(const char *const language, const char* const imagePath, const char *outPath)
+{
+  printf("Doing %s\n", imagePath);
+
+  FILE *outFile;
+  bool status = true;
+  if((outFile = fopen(outPath,"w")) == NULL) {
+    fprintf(stderr, "Could not open output file.\n");
+    return false;
+  }
+
+  fprintf(outFile, "OCR output for image %s:\n", imagePath);
+
+
+  Pix *image = pixRead(imagePath);
+  tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+  api->Init(NULL, language);
+  api->SetImage(image);
+  api->SetVariable("save_blob_choices", "T");
+  api->SetRectangle(37, 228, 548, 31);
+  api->Recognize(NULL);
+
+  tesseract::ResultIterator* ri = api->GetIterator();
+  tesseract::PageIteratorLevel level = tesseract::RIL_SYMBOL;
+  if(ri != 0) {
+      do {
+          const char* symbol = ri->GetUTF8Text(level);
+          float conf = ri->Confidence(level);
+          if(symbol != 0) {
+	    fprintf(outFile, "symbol %s, conf: %f", symbol, conf);
+              bool indent = false;
+              tesseract::ChoiceIterator ci(*ri);
+              do {
+                  if (indent) printf("\t\t ");
+                  fprintf(outFile, "\t- ");
+                  const char* choice = ci.GetUTF8Text();
+                  fprintf(outFile, "%s conf: %f\n", choice, ci.Confidence());
+                  indent = true;
+              } while(ci.Next());
+          }
+          fprintf(outFile, "---------------------------------------------\n");
+          delete[] symbol;
+      } while((ri->Next(level)));
+  }
+
+  return true;
+}
+
+int die()
+{
+  printf("Dead!!!");
+  exit(EXIT_FAILURE);
+}
+
+
+
+int main()
+{
+  // Open input image with leptonica library
+  ocr("eng",
+      "./images/Paragraph.tif",
+      "./outputs/Paragraph_symbol.txt") || die();
+
+  ocr("chi_tra",
+      "./images/chinese-tradition-0pic.png",
+      "./outputs/chinese-tradition-0pic-chi_tra_symbol.txt") || die();
+
+  ocr("chi_sim",
+      "./images/chinese-tradition-0pic.png",
+      "./outputs/chinese-tradition-0pic-chi_sim_symbol.txt") || die();
+}
