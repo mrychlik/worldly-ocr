17f47386089f486d19bbe45fa7e08777d8a967a1
Marek Rychlik <marek@marekrychlik.com>
2019-02-22 20:08:32 -0700

    *** empty log message ***

--- TesseractExamples/main1.cpp
+++ TesseractExamples/main1.cpp
@@ -19,45 +19,57 @@ const char LANGUAGE[] = "chi_tra";
 
 int ocr(const char *const language, const char* const imagePath, const char *outPath)
 {
-    char *outText;
+  printf("Doing %s\n", imagePath);
+  char *outText;
 
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-    // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, language)) {
-        fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
-    }
+  tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+  // Initialize tesseract-ocr with English, without specifying tessdata path
+  if (api->Init(NULL, language)) {
+    fprintf(stderr, "Could not initialize tesseract.\n");
+    exit(1);
+  }
 
-    // Open input image with leptonica library
-    // Pix *image = pixRead("./images/Paragraph.tif");
-    Pix *image = pixRead(imagePath);
+  // Open input image with leptonica library
+  // Pix *image = pixRead("./images/Paragraph.tif");
+  Pix *image = pixRead(imagePath);
 
-    api->SetImage(image);
-    // Get OCR result
-    outText = api->GetUTF8Text();
+  api->SetImage(image);
+  // Get OCR result
+  outText = api->GetUTF8Text();
     
-    FILE *outFile;
-    int status;
+  FILE *outFile;
+  int status;
 
-    if((outFile = fopen(outPath,"r")) != NULL) {
-      fprintf(outFile, "OCR output for image %s:\n%s", imagePath, outText);
-      fclose(outFile);
-    } else {
-      status = 1;
-    }
+  if((outFile = fopen(outPath,"r")) != NULL) {
+    fprintf(outFile, "OCR output for image %s:\n%s", imagePath, outText);
+    fclose(outFile);
+  } else {
+    status = 1;
+  }
 
-    // Destroy used object and release memory
-    api->End();
-    delete [] outText;
-    pixDestroy(&image);
+  // Destroy used object and release memory
+  api->End();
+  delete [] outText;
+  pixDestroy(&image);
 
-    return status;
+  return status;
+}
+
+
+int die()
+{
+  exit(EXIT_FAILURE);
 }
 
 
 int main()
 {
   // Open input image with leptonica library
-  ocr("eng", "./images/Paragraph.tif", "./outputs/Paragraph.txt");
-  ocr("chi_tra", "./images/chinese-tradition-0pic.png", "./outputs/chinese-tradition-0pic-chi_tra.txt");
+  ocr("eng",
+      "./images/Paragraph.tif",
+      "./outputs/Paragraph.txt") == 0 || die();
+
+  ocr("chi_tra",
+      "./images/chinese-tradition-0pic.png",
+      "./outputs/chinese-tradition-0pic-chi_tra.txt") || die();
 }
