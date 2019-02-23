8fcc5e8189ff2ee791539c010bedd2ff5e062983
Marek Rychlik <marek@marekrychlik.com>
2019-02-22 20:04:24 -0700

    *** empty log message ***

--- TesseractExamples/main1.cpp
+++ TesseractExamples/main1.cpp
@@ -17,32 +17,47 @@
 const char LANGUAGE[] = "chi_tra";
 
 
-int main()
+int ocr(const char *const language, const char* const imagePath, const char *outPath)
 {
     char *outText;
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, LANGUAGE)) {
+    if (api->Init(NULL, language)) {
         fprintf(stderr, "Could not initialize tesseract.\n");
         exit(1);
     }
 
     // Open input image with leptonica library
     // Pix *image = pixRead("./images/Paragraph.tif");
-    Pix *image = pixRead("./images/chinese-tradition-0pic.png");
-
-
+    Pix *image = pixRead(imagePath);
 
     api->SetImage(image);
     // Get OCR result
     outText = api->GetUTF8Text();
-    printf("OCR output:\n%s", outText);
+    
+    FILE *outFile;
+    int status;
+
+    if((outFile = fopen(outPath,"r")) != NULL) {
+      fprintf(outFile, "OCR output for image %s:\n%s", imagePath, outText);
+      close(outFile);
+    } else {
+      status = 1;
+    }
 
     // Destroy used object and release memory
     api->End();
     delete [] outText;
     pixDestroy(&image);
 
-    return 0;
+    return status;
+}
+
+
+int main()
+{
+  // Open input image with leptonica library
+  // Pix *image = pixRead("./images/Paragraph.tif");
+  ocr("chi_tra", "./images/chinese-tradition-0pic.png", "./outputs/chinese-tradition-0pic-chi_tra.txt");
 }
