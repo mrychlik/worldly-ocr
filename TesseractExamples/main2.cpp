98e01e27a8603fa1aa4d84e4d28b01242437d00b
Marek Rychlik <marek@marekrychlik.com>
2019-02-23 09:09:08 -0700

    *** empty log message ***

--- TesseractExamples/main2.cpp
+++ TesseractExamples/main2.cpp
@@ -16,20 +16,29 @@ bool ocr(const char *const language, const char* const imagePath, const char *ou
 {
   bool status = true;
 
-  Pix *image = pixRead("/usr/src/tesseract/testing/phototest.tif");
+  Pix *image = pixRead(imagePath);
   tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
   api->Init(NULL, language);
   api->SetImage(image);
   Boxa* boxes = api->GetComponentImages(tesseract::RIL_TEXTLINE, true, NULL, NULL);
   printf("Found %d textline image components.\n", boxes->n);
-  for (int i = 0; i < boxes->n; i++) {
-    BOX* box = boxaGetBox(boxes, i, L_CLONE);
-    api->SetRectangle(box->x, box->y, box->w, box->h);
-    char* ocrResult = api->GetUTF8Text();
-    int conf = api->MeanTextConf();
-    fprintf(stdout, "Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s",
-	    i, box->x, box->y, box->w, box->h, conf, ocrResult);
+
+  FILE *outFile;
+  if((outFile = fopen(outPath,"w")) != NULL) {
+    fprintf(outFile, "OCR output for image %s:\n", imagePath);
+    for (int i = 0; i < boxes->n; i++) {
+      BOX* box = boxaGetBox(boxes, i, L_CLONE);
+      api->SetRectangle(box->x, box->y, box->w, box->h);
+      char* ocrResult = api->GetUTF8Text();
+      int conf = api->MeanTextConf();
+      fprintf(outFile, "Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s",
+	      i, box->x, box->y, box->w, box->h, conf, ocrResult);
+    }
+    fclose(outFile);
+  } else {
+    status = false;
   }
+
   return status;
 }
 
