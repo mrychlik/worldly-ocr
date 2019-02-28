28dd4ad437b4d6711d9e345f18e8aaae447fda83
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 10:46:38 -0700

    *** empty log message ***

--- TesseractHack/box_script.m
+++ TesseractHack/box_script.m
@@ -6,7 +6,7 @@ fh = fopen('box_file.txt','w');
 for j=1:length(objects)
     b = objects(j).BoundingBox;
     x=floor(b(1)); y=floor(b(2)); w=ceil(b(3)); h=ceil(b(4));
-    fprintf(fh, '%c %d %d %d %d 0\n', x, y, x+w, y+h);
+    fprintf(fh, '%c %d %d %d %d 0\n', 'X', x, y, x+w, y+h);
 end
 
 fclose(fh);
