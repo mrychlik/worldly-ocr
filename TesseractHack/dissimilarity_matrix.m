f36eb61a3d43d0ce6291dc33adb4093035fb9f5c
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 15:17:25 -0700

    *** empty log message ***

---
+++ TesseractHack/dissimilarity_matrix.m
@@ -0,0 +1,9 @@
+function D = dissimilarity_matrix(objects)
+n = length(objects);
+D = zeros(n,n);
+for j=1:n
+    for k=j:n
+        D(j,k) = dissimilarity(objects(j),objects(k));
+    end
+end
+D = D + D';
