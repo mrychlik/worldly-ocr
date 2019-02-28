bd90dcc704ce90b6139a3ee5974ff7a2dbb5887b
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 15:18:10 -0700

    *** empty log message ***

--- TesseractHack/dissimilarity.m
+++ TesseractHack/dissimilarity.m
@@ -1,7 +1,7 @@
 function Dis = dissimilarity(obj1, obj2)
     F1 = fft2(obj1.grayscaleimage);
     F2 = fft2(obj2.grayscaleimage);
-    G = (F1 .* conj(F2)) / (eps + abs(F1) .* abs(F2));
+    G = (F1 .* conj(F2)) ./ (eps + abs(F1) .* abs(F2));
     H = abs(ifft2(G));
     Dis = entropy(H(:));
 end
