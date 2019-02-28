689d5dd10cd44cd12099b5bc22c610357765454f
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 14:24:33 -0700

    *** empty log message ***

--- TesseractHack/label_script.m
+++ TesseractHack/label_script.m
@@ -8,9 +8,24 @@ else
 end
 
 
+
+max_h = 0;
+max_w = 0;
+
+for j=1:length(objects)
+    [h,w] = size(objects(j).bwimage)
+    max_h = max(max_h, h);
+    max_w = max(max_w, w);
+end
+    
+    
+
+
+
+
 for j=1:length(objects)
     objects(j).grayscaleimage = objects(j).bwimage .* 255;
     objects(j).char = ' ';
 end
 
-label_objects(objects);
+%label_objects(objects);
