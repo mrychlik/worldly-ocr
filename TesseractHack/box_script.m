1976b2519cf44885a25eb3fa8e9f6a24ae746ae5
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 09:50:09 -0700

    *** empty log message ***

--- TesseractHack/box_script.m
+++ TesseractHack/box_script.m
@@ -1,6 +1,9 @@
 [I,cmap]=imread('BoxFileExample/39097174-8ee9c5d4-4676-11e8-9023-a9657006eabc.png');
 [objects,lines]=bounding_boxes(~I);
 
-for j=1:length(objects)
-    imagesc(objects(j).bwimage), drawnow, pause(.1);
+%for j=1:length(objects)
+for j=1
+    %imagesc(objects(j).bwimage), drawnow, pause(.1);
+    b = objects(j).BoundingBox;
+    x=b(1); y=b(2); w=b(3); h=b(4);
 end
