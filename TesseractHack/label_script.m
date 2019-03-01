ad9086094b6fe2dbd6f92250110773b58edcc9bb
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 18:14:01 -0700

    *** empty log message ***

--- TesseractHack/label_script.m
+++ TesseractHack/label_script.m
@@ -26,7 +26,7 @@ for j=1:length(objects)
     y = round((max_h - h)/2);
     J( (y+1):(y+h), (x+1):(x+w) ) = BW .* 255;
     objects(j).grayscaleimage = J;
-    objects(j).char = ' ';
+    objects(j).char = '';
 end
 
 label_objects(objects);
