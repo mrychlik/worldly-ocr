b121fd96888ec115e18c58094196ef1ebdfa68bf
Marek Rychlik <marek@marekrychlik.com>
2019-02-28 17:20:48 -0700

    *** empty log message ***

--- TesseractHack/dissimilarity.m
+++ TesseractHack/dissimilarity.m
@@ -1,8 +1,13 @@
-function Dis = dissimilarity(obj1, obj2)
+function Dis = dissimilarity(obj1, obj2, k)
     F1 = fft2(obj1.grayscaleimage);
     F2 = fft2(obj2.grayscaleimage);
     G = (F1 .* conj(F2)) ./ (eps + abs(F1) .* abs(F2));
     H = abs(ifft2(G));
-    K = sort(H(:));
-    Dis=sum(K((end-7):end));
+    F = zeros(size(H));
+    F(1:2,1:2) = 1;
+    F(1:2,(end-1):end) = 1;
+    F((end-1):end,1:2) = 1;
+    F((end-1):end,(end-1):end) = 1;
+    K = ifft2(fft2(H) .* fft(F));
+    Dis=max(K(:));
 end
