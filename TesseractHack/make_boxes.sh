d3a8be821f0f3a0a9b5d39bac8a835c488d3d8dc
Marek Rychlik <marek@marekrychlik.com>
2019-03-02 18:35:01 -0700

    *** empty log message ***

--- TesseractHack/make_boxes.sh
+++ TesseractHack/make_boxes.sh
@@ -1,9 +1,10 @@
 TRAINING_SCRIPT=/home/marek/TESSERACT/tesseract/src/training/tesstrain.sh
 LANGDATA_DIR=/home/marek/TESSERACT/langdata
 TESSDATA_DIR=/home/marek/TESSERACT/tesseract/tessdata
+FONTDIR=/home/marek/.wine/drive_c/windows/Fonts
 
 # List of fonts to train on
-MY_FONTS=(
+SOME_FONTS=(
     "Courier New Bold" \
     "Courier New Bold Italic" \
     "Courier New Italic" \
@@ -35,8 +36,9 @@ MY_FONTS=(
 )
 
 
+MY_FONTS="Times New Roman"
 
-$TRAINING_SCRIPT --fonts_dir /usr/share/fonts --lang eng --linedata_only \
-		 --fontlist  $MY_FONTS \
+
+$TRAINING_SCRIPT --fonts_dir $FONTDIR --lang eng --linedata_only \
 		 --noextract_font_properties --langdata_dir $LANGDATA_DIR \
 		 --tessdata_dir  $TESSDATA_DIR --output_dir ~/tesstutorial/engtrain
