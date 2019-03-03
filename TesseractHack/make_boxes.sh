2e331e1c357750ae8b56dfee802f3f8657970be3
Marek Rychlik <marek@marekrychlik.com>
2019-03-02 18:58:09 -0700

    *** empty log message ***

--- TesseractHack/make_boxes.sh
+++ TesseractHack/make_boxes.sh
@@ -1,44 +1,26 @@
-TRAINING_SCRIPT=/home/marek/TESSERACT/tesseract/src/training/tesstrain.sh
-LANGDATA_DIR=/home/marek/TESSERACT/langdata
-TESSDATA_DIR=/home/marek/TESSERACT/tesseract/tessdata
+!/bin/bash
 FONTDIR=/home/marek/.wine/drive_c/windows/Fonts
+TESSERACT_HOME=/home/marek/TESSERACT/tesseract
+
+cd $TESSERACT_HOME
+
+LANGDATA_DIR='../langdata'
+TESSDATA_DIR='./tessdata'
+
+
+
 
 # List of fonts to train on
-SOME_FONTS=(
-    "Courier New Bold" \
-    "Courier New Bold Italic" \
-    "Courier New Italic" \
-    "Courier New" \
-    "Times New Roman, Bold" \
-    "Times New Roman, Bold Italic" \
-    "Times New Roman, Italic" \
-    "Times New Roman," \
-    "Georgia Bold" \
-    "Georgia Italic" \
-    "Georgia" \
-    "Georgia Bold Italic" \
-    "Trebuchet MS Bold" \
-    "Trebuchet MS Bold Italic" \
-    "Trebuchet MS Italic" \
-    "Trebuchet MS" \
-    "Verdana Bold" \
-    "Verdana Italic" \
-    "Verdana" \
-    "Verdana Bold Italic" \
-    "URW Bookman L Bold" \
-    "URW Bookman L Italic" \
-    "URW Bookman L Bold Italic" \
-    "Century Schoolbook L Bold" \
-    "Century Schoolbook L Italic" \
-    "Century Schoolbook L Bold Italic" \
-    "Century Schoolbook L Medium" \
-    "DejaVu Sans Ultra-Light" \
+MYFONTS=(
+    "Arial"
+    "Verdana"
 )
 
 
-MY_FONTS="Times New Roman"
 
 
-$TRAINING_SCRIPT --fonts_dir $FONTDIR --lang eng --linedata_only \
+
+./src/training/tesstrain.sh --fonts_dir $FONTDIR --lang eng --linedata_only \
+		 --fontlist $MYFONTS \
 		 --noextract_font_properties --langdata_dir $LANGDATA_DIR \
 		 --tessdata_dir  $TESSDATA_DIR --output_dir ~/tesstutorial/engtrain
