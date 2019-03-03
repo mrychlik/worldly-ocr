5f0b26ee1f0be814ce4309f2c08b41d91d963e57
Marek Rychlik <marek@marekrychlik.com>
2019-03-03 14:33:16 -0700

    *** empty log message ***

--- TesseractHack/fourier_clustering_script.m
+++ TesseractHack/fourier_clustering_script.m
@@ -9,6 +9,16 @@ else
     save(savefile,'objects','lines');
 end
 
+fprintf('Determining maximum object size...')
+max_h = 0;
+max_w = 0;
+for j=1:length(objects)
+    [h,w] = size(objects(j).bwimage);
+    max_h = max(max_h, h);
+    max_w = max(max_w, w);
+end
+fprintf('Max. height: %g, max. width: %g', max_h, max_w);
+
 wb = waitbar(0, 'Cropping/centering objects and converting to grayscale...');
 num_objects = length(objects);
 for j=1:num_objects;
