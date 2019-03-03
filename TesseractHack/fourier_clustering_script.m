4c3a4dbf50fc51ba771cc1d3cdb2b51dffc0ccfe
Marek Rychlik <marek@marekrychlik.com>
2019-03-03 14:16:01 -0700

    *** empty log message ***

--- TesseractHack/fourier_clustering_script.m
+++ TesseractHack/fourier_clustering_script.m
@@ -9,6 +9,22 @@ else
     save(savefile,'objects','lines');
 end
 
+wb = waitbar(0, 'Cropping/centering objects and converting to grayscale...');
+num_objects = length(objects);
+for j=1:num_objects;
+    waitbar(j/num_objects, wb);
+    J = zeros([max_h,max_w],'uint8');
+    BW = objects(j).bwimage;
+    [h,w] = size(BW);
+    x = round((max_w - w)/2);
+    y = round((max_h - h)/2);
+    J( (y+1):(y+h), (x+1):(x+w) ) = BW .* 255;
+    objects(j).grayscaleimage = J;
+    objects(j).char = ' ';
+end
+close(wb);
+
+
 [class_idx, class_num, class_reps] = fourier_clustering(objects)
 
 % Label class representatives
