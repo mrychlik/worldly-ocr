d027391a2f3a6b75dd6f6ac44a38e03bc9ff3d30
Marek Rychlik <marek@marekrychlik.com>
2019-03-03 14:09:23 -0700

    *** empty log message ***

--- TesseractHack/fourier_clustering_script.m
+++ TesseractHack/fourier_clustering_script.m
@@ -9,71 +9,7 @@ else
     save(savefile,'objects','lines');
 end
 
-fprintf('Determining maximum object size...')
-max_h = 0;
-max_w = 0;
-for j=1:length(objects)
-    [h,w] = size(objects(j).bwimage);
-    max_h = max(max_h, h);
-    max_w = max(max_w, w);
-end
-fprintf('Max. height: %g, max. width: %g', max_h, max_w);
-
-
-wb = waitbar(0, 'Cropping/centering objects and converting to grayscale...');
-num_objects = length(objects);
-for j=1:num_objects;
-    waitbar(j/num_objects, wb);
-    J = zeros([max_h,max_w],'uint8');
-    BW = objects(j).bwimage;
-    [h,w] = size(BW);
-    x = round((max_w - w)/2);
-    y = round((max_h - h)/2);
-    J( (y+1):(y+h), (x+1):(x+w) ) = BW .* 255;
-    objects(j).grayscaleimage = J;
-    objects(j).char = ' ';
-end
-close(wb);
-
-% Find equivalent objects
-n = length(objects);
-Q = zeros(n,n);
-threshold = .75;
-classified = zeros(1,n);
-class_reps = zeros(1,n);
-for j = 1:(n-1)
-    if classified(j)
-        continue;
-    end;
-    classified(j)=1;
-    class_reps(j)=1;
-    fprintf('New object: %d, Number of classes: %d\n', j, length(find(class_reps)));
-    for k = (j+1):n
-        D = dissimilarity(objects(j), objects(k));
-        if classified(k)
-            continue;
-        end
-        if D > threshold
-            Q(j,k)=1;
-            classified(k) = 1;
-        end
-    end
-end
-
-% Visualize classes
-imagesc(Q),drawnow;
-for j = 1:n
-    if class_reps(j)
-        idx = [j,find(Q(j,:))];
-        s = length(idx);
-        t = ceil(sqrt(s));
-        for k=1:s
-            subplot(t,t,k), imagesc(objects(idx(k)).grayscaleimage);
-        end
-        drawnow, pause(1);
-        clf;
-    end
-end
+[class_idx, class_num, class_reps] = fourier_clustering(objects)
 
 % Label class representatives
 reps = objects(find(class_reps));
