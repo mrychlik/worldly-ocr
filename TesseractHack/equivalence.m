b6f533732e7c5aa636a93f829b62a177d62ec402
Marek Rychlik <marek@marekrychlik.com>
2019-03-03 13:32:51 -0700

    *** empty log message ***

--- TesseractHack/equivalence.m
+++ TesseractHack/equivalence.m
@@ -2,13 +2,12 @@
 savefile=fullfile('Cache','Paragraph.mat');
 
 if exist(savefile,'file') == 2
+    fprintf('Loading savefile %s', savefile);
     load(savefile)
 else
     [objects,lines]=bounding_boxes(~I);
     save(savefile,'objects','lines');
 end
-
-
 
 max_h = 0;
 max_w = 0;
