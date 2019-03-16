810775b3a72ba4c4581c4ebcad929aa99270b6fb
Marek Rychlik <marek@marekrychlik.com>
2019-03-16 09:53:22 -0700

    *** empty log message ***

--- WLanguageWithLSTM/myClassificationLayer.m
+++ WLanguageWithLSTM/myClassificationLayer.m
@@ -1,52 +1,58 @@
 classdef myClassificationLayer < nnet.layer.ClassificationLayer
 % A layer to test what is passed on from previous layers
 % Implements zero loss function.
-        
+    
     properties
         % (Optional) Layer properties.
 
         % Layer properties go here.
     end
- 
+    
     methods
         function layer = myClassificationLayer()           
-            % (Optional) Create a myClassificationLayer.
+        % (Optional) Create a myClassificationLayer.
 
-            % Layer constructor function goes here.
-                layer.Name = 'Dummy Classification Layer'
+        % Layer constructor function goes here.
+            layer.Name = 'Dummy Classification Layer'
         end
 
         function loss = forwardLoss(layer, Y, T)
-            % Return the loss between the predictions Y and the 
-            % training targets T.
-            %
-            % Inputs:
-            %         layer - Output layer
-            %         Y     – Predictions made by network
-            %         T     – Training targets
-            %
-            % Output:
-            %         loss  - Loss between Y and T
+        % Return the loss between the predictions Y and the 
+        % training targets T.
+        %
+        % Inputs:
+        %         layer - Output layer
+        %         Y     – Predictions made by network
+        %         T     – Training targets
+        %
+        % Output:
+        %         loss  - Loss between Y and T
 
-            % Layer forward loss function goes here.
-                display(Y);
-                pause(5);
-                loss = single(0);
+        % Layer forward loss function goes here.
+            1;    
+            % Y should be KxN, where K is the number of classes
+            % from the soft-max layer, and N is the value of
+            % the 'MiniBatchSize' parameter.
+            display(Y);
+            pause(5);
+
+            % Trivial loss
+            loss = single(0);
         end
         
         function dLdY = backwardLoss(layer, Y, T)
-            % Backward propagate the derivative of the loss function.
-            %
-            % Inputs:
-            %         layer - Output layer
-            %         Y     – Predictions made by network
-            %         T     – Training targets
-            %
-            % Output:
-            %         dLdY  - Derivative of the loss with respect to the predictions Y
+        % Backward propagate the derivative of the loss function.
+        %
+        % Inputs:
+        %         layer - Output layer
+        %         Y     – Predictions made by network
+        %         T     – Training targets
+        %
+        % Output:
+        %         dLdY  - Derivative of the loss with respect to the predictions Y
 
-            % Layer backward loss function goes here.
-                dLdY = zeros(size(Y),'single');
+        % Layer backward loss function goes here.
+            dLdY = zeros(size(Y),'single');
         end
     end
 end
