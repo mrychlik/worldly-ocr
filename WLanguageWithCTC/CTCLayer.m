0797a16d6570157cee76891b5f8e021a13f2bba9
Marek Rychlik <marek@marekrychlik.com>
2019-03-17 10:42:33 -0700

    *** empty log message ***

--- WLanguageWithCTC/CTCLayer.m
+++ WLanguageWithCTC/CTCLayer.m
@@ -50,6 +50,7 @@ classdef CTCLayer < nnet.layer.ClassificationLayer
 
             for n = 1 : N
                 T1 = squeeze(T(:,n,:);
+
                 alpha = update_alpha(Y, T1));
 
                 p = alpha(S, length(lPrime)); 
