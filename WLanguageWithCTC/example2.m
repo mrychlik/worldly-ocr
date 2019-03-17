37a1dd4680c6ae744cd28dd895c7ac3103c7baca
Marek Rychlik <marek@marekrychlik.com>
2019-03-16 21:19:24 -0700

    *** empty log message ***

--- WLanguageWithCTC/example2.m
+++ WLanguageWithCTC/example2.m
@@ -6,7 +6,7 @@
 %
 % NOTE: The loss is set to 0, so nothing useful happens in regard to training.
 %
-[XTrain, YTrain] = prepareDataTrain(1);
+[XTrain, YTrain] = prepareDataTrain(1000, 10, 1);
 
 numFeatures = 3;
 numHiddenUnits = 9;
