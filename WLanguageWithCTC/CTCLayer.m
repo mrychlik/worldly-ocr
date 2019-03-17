0ac896b76061628c86b1da48ea91499b291ec1cf
Marek Rychlik <marek@marekrychlik.com>
2019-03-17 09:59:01 -0700

    *** empty log message ***

--- WLanguageWithCTC/CTCLayer.m
+++ WLanguageWithCTC/CTCLayer.m
@@ -165,7 +165,8 @@ classdef CTCLayer < nnet.layer.ClassificationLayer
                     for k=1:blank
                         for s=1:length(lPrime)
                             if lPrime(s) == k
-                                dp(k,t) = dp(k,t) + alpha(t,s).*beta(t, s)./Y(k, n, t).^2;
+                                dp(k,t) = dp(k,t) + alpha(t,s) .* ...
+                                          beta(t, s)./Y(k, n, t).^2;
                             end
                         end
                     end
