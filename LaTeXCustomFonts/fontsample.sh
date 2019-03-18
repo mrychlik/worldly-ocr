dcf9e01d2b102acb2a0830dc85ada968207975fa
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 10:32:28 -0700

    *** empty log message ***

--- LaTeXCustomFonts/fontsample.sh
+++ LaTeXCustomFonts/fontsample.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 #lualatex forpdflatex.tex
+
+# Run LaTeX (must be lualatex!) on the here document
+
 lualatex <<EOF
 \documentclass{article}
 % This is for LuaTeX or XeTeX
@@ -15,7 +18,7 @@ The quick brown fox jumps over the lazy dog
 \end{document}
 EOF
 
-
+# Convert the document to PPM
 pdftoppm -r 600 texput.pdf texput
 #The command below does not crop properly
 #convert -trim forpdflatex-*.ppm forpdflatex_trimmed.ppm
@@ -23,6 +26,7 @@ pdftoppm -r 600 texput.pdf texput
 # This implements GIMP zealous crop programmatically
 #./crop.sh forpdflatex-1.ppm forpdflatex.tiff
 
+# Run GIMP on putput to produce a file texput.tiff (a TIFF file)
 input=texput-1.ppm
 output=texput.tiff
 gimp -i -b "(crop-ppm \"$input\" \"$output\")" -b "(gimp-quit 0)"
