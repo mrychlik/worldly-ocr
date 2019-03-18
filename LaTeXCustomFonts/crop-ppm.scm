bc32c6d16ec26b5fc724977fcf46594bd613bc8e
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 10:11:50 -0700

    *** empty log message ***

--- LaTeXCustomFonts/crop-ppm.scm
+++ LaTeXCustomFonts/crop-ppm.scm
@@ -10,7 +10,7 @@
   (plug-in-zealouscrop RUN-NONINTERACTIVE image drawable)
 
   ; save in original TIFF format
-  (file-tiff-save RUN-NONINTERACTIVE image drawable output-filename output-filename 0)
+  (file-tiff-save RUN-NONINTERACTIVE image drawable output-filename output-filename 1)
 
   ; clean up the image
   (gimp-image-delete image)
