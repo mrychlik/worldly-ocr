ace78db965f399d69326d854304901d625381f4d
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 10:19:30 -0700

    *** empty log message ***

--- LaTeXCustomFonts/fontsample.sh
+++ LaTeXCustomFonts/fontsample.sh
@@ -2,5 +2,9 @@
 lualatex forpdflatex.tex
 
 pdftoppm -r 600 forpdflatex.pdf forpdflatex
+#The command below does not crop properly
+#convert -trim forpdflatex-*.ppm forpdflatex_trimmed.ppm
+
+# This implements GIMP zealous crop programmatically
 ./crop.sh forpdflatex-1.ppm forpdflatex.tiff 
 
