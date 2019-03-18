22a6662a15fec7e557922f489fc5cfe319325daa
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 08:56:39 -0700

    *** empty log message ***

--- LaTeXCustomFonts/fontsample.sh
+++ LaTeXCustomFonts/fontsample.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 lualatex forpdflatex.tex
 pdftoppm -r 600 forpdflatex.pdf forpdflatex.ppm
-convert -quality 1 -trim forpdflatex.ppm forpdflatex_trimmed.ppm
+convert -trim forpdflatex.ppm forpdflatex_trimmed.ppm
