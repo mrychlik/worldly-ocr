20769fc3df75714b11eb2eab4eb25db8eae31af8
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 10:25:40 -0700

    *** empty log message ***

--- LaTeXCustomFonts/fontsample.sh
+++ LaTeXCustomFonts/fontsample.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
-lualatex forpdflatex.tex
+if [ $# -le 0 ]; then
+    echo
+    echo "Usage: $(basename $0) input.[ppm|pdf|...] output.ppm"
+    echo
+    echo "  This script uses gimp to autocrop PPM files and"
+    echo "  save them to PPM format.  You must have"
+    echo "  crop.scm installed in your gimp "
+    echo "  scripts directory."
+    echo
+    exit 1
+fi
+
+
+#lualatex forpdflatex.tex
+lualatex <<EOF
+\documentclass{article}
+% This is for LuaTeX or XeTeX
+\usepackage{fontspec}
+%\pdfmapfile{+myfont.map}
+\setmainfont{[Lunafreya.ttf]}
+\begin{document}
+% Contains all letters of the English alphabet
+\begin{LARGE}
+The quick brown fox jumps over the lazy dog
+\end{LARGE}
+\end{document}
+EOF
+
 
 pdftoppm -r 600 forpdflatex.pdf forpdflatex
 #The command below does not crop properly
