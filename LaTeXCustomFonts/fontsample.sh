35953abc8cfd593198a3ee7cd87b42bc33f1120e
Marek Rychlik <marek@marekrychlik.com>
2019-03-18 10:29:26 -0700

    *** empty log message ***

--- LaTeXCustomFonts/fontsample.sh
+++ LaTeXCustomFonts/fontsample.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-if [ $# -le 0 ]; then
-    echo
-    echo "Usage: $(basename $0) input.[ppm|pdf|...] output.ppm"
-    echo
-    echo "  This script uses gimp to autocrop PPM files and"
-    echo "  save them to PPM format.  You must have"
-    echo "  crop.scm installed in your gimp "
-    echo "  scripts directory."
-    echo
-    exit 1
-fi
-
 
 #lualatex forpdflatex.tex
 lualatex <<EOF
@@ -28,14 +16,14 @@ The quick brown fox jumps over the lazy dog
 EOF
 
 
-pdftoppm -r 600 forpdflatex.pdf forpdflatex
+pdftoppm -r 600 texput.pdf texput
 #The command below does not crop properly
 #convert -trim forpdflatex-*.ppm forpdflatex_trimmed.ppm
 
 # This implements GIMP zealous crop programmatically
 #./crop.sh forpdflatex-1.ppm forpdflatex.tiff
 
-input=forpdflatex-1.ppm
-output=forpdflatex.tiff
+input=texput-1.ppm
+output=texput.tiff
 gimp -i -b "(crop-ppm \"$input\" \"$output\")" -b "(gimp-quit 0)"
 
