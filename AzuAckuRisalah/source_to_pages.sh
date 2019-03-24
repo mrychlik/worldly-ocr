992489fcd53ebeaa70bea021af90feecdcfcad0a
Marek Rychlik <marek@marekrychlik.com>
2019-03-24 09:48:19 -0700

    *** empty log message ***

--- AzuAckuRisalah/source_to_pages.sh
+++ AzuAckuRisalah/source_to_pages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Use this script to convert the source book to page images
+# Use this script to convert the source book to page images in PPM format
 cd Pages
 pdftoppm -r 300 ../Source/azu_acku_risalah_ds371_2_zay48_1990_w.pdf page
