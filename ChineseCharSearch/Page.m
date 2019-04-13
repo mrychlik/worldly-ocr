1f25d15139f59f033a4f55311a1f340fa42b973c
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 10:19:17 -0700

    *** empty log message ***

--- ChineseCharSearch/Page.m
+++ ChineseCharSearch/Page.m
@@ -170,7 +170,7 @@ classdef Page
             BW=im2bw(obj.I,obj.bw_threshold);
             obj.top_margin_size;
             BW(1:obj.top_margin_size,:)=0;
-            BW((end-obj.margin_sz):end,:)=0;    
+            BW((end-obj.margin_size):end,:)=0;    
             if strcmp(obj.Display,'on')
                 imshow(BW);
             end
