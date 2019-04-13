2bc68c9d859138ec0aee473136dcfd68ebcfe7cb
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 10:17:13 -0700

    *** empty log message ***

--- ChineseCharSearch/source_to_pages.sh
+++ ChineseCharSearch/source_to_pages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use this script to convert the source book to page images in PNG format
 cd Pages
-pdftopng -r 400 ../../Data/06061317.cn.pdf 06061317.cn
+pdftopng -r 300 ../../Data/06061317.cn.pdf 06061317.cn
