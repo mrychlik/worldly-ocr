54aaf16ddf3475a0337a26f1f4605aed4ae135ab
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 07:26:54 -0700

    *** empty log message ***

--- ChineseCharSearch/source_to_pages.sh
+++ ChineseCharSearch/source_to_pages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use this script to convert the source book to page images in PPM format
 cd Pages
-pdftopng -r 300 ../../worldly-ocr/Data/06061317.cn.pdf page
+pdftopng -r 300 ../../Data/06061317.cn.pdf page
