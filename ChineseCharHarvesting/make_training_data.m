094906e16004ea36282b3d323b0bc7ef5b36d22b
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 17:49:03 -0700

    *** empty log message ***

--- ChineseCharHarvesting/make_training_data.m
+++ ChineseCharHarvesting/make_training_data.m
@@ -1,6 +1,16 @@
 bw_chardir='BWChars';
-for char_count=1:16002
-    BW=imwread(fullfile(bw_chardir,sprintf('char%05d.pbm', char_count));
-    imshow(BW),drawnow;
-    pause(1);
+% Compuute common bounding box
+N=16002;
+BW=cell(N,1);
+max_h = 0; max_w = 0;
+bh=waitbar(0,'Computing common bounding box size...');
+for char_count=1:N
+    waitbar(char_count/N,bh);
+    imfile=fullfile(bw_chardir,sprintf('char%05d.pbm', char_count));
+    BW{char_count}=imread(imfile);
+    %imshow(BW{char_count}),drawnow;
+    [h,w]=size(BW{char_count});
+    max_h = max(h, max_h);
+    max_2 = max(h, max_w);    
 end
+close(bh);
