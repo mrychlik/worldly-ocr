8d59e75440dc09c5ad0a5215b8873ab233f2e2a7
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 18:03:31 -0700

    *** empty log message ***

--- ChineseCharHarvesting/make_training_data.m
+++ ChineseCharHarvesting/make_training_data.m
@@ -11,14 +11,16 @@ for char_count=1:N
     %imshow(BW{char_count}),drawnow;
     [h,w]=size(BW{char_count});
     max_h = max(h, max_h);
-    max_w = max(h, max_w);    
+    max_w = max(w, max_w);    
 end
 close(bh);
 
-I=zeros(max_h,max_w,N);
+X=zeros(max_h,max_w,N);
 for char_count=1:N
     [h,w]=size(BW{char_count});
     y_off=round((max_h-h)/2);
     x_off=round((max_w-w)/2);
-    I(y_off:(y_off+h),x_off:(x_off+w),char_count)=BW{char_count};
+    X((y_off+1):(y_off+h),(x_off+1):(x_off+w),char_count)=BW{char_count};
 end
+
+save('training_data.mat','X','-v7.3');
