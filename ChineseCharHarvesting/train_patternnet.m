ad4f50cf09b638f01081dcd8ba81ee7d6f03cf18
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 21:24:40 -0700

    *** empty log message ***

--- ChineseCharHarvesting/train_patternnet.m
+++ ChineseCharHarvesting/train_patternnet.m
@@ -1,8 +1,11 @@
-function [Y,NErrors,W] = train_patternnet(X, T, num_epochs)
-% NERRORS = TRAIN_PATTERNNET(X, T, NUM_EPOCHS)    trains
+function [Y,NErrors,W] = train_patternnet(X, T, num_epochs, minibatch_size)
+% NERRORS = TRAIN_PATTERNNET(X, T, NUM_EPOCHS, MINIBATCH_SIZE) trains
 % a pattennet with H hidden neurons.
+    if nargin < 3; num_epochs = 100; end
+    if nargin < 4; minibatch_size = 10; end
+
     T = sparse(T);                      % One-hot encoding is really wasteful
-    min_eta = 1e-5;                     % Stop if learning rate drops below
+    eta = 5e-2;                         % Stop if learning rate drops below
     alpha = 1e-1;                       % Regularizer constant
 
     assert(size(X,2) == size(T,2), ['Inconsistent number of samples in ' ...
@@ -12,65 +15,41 @@ function [Y,NErrors,W] = train_patternnet(X, T, num_epochs)
     D = size(X, 1);                     % Dimension of data
     N = size(X, 2);                     % Number of samples
     C = size(T, 1);                     % Number of  classes
-
     W = spalloc(C,D,10);                % Starting weihgts
-
-    Y = softmax(W * X);             % Compute activations
-    %% Update gradient
-    E = T - Y;
-    gradLoss = -E * X' + alpha * W;;       % Gradient
-
-    eta = 1 /(eps + norm(gradLoss));    % Initial learning rate
-
     G = loss(W,Y,T,alpha);              % Test on the original sample
     Gn = [G];
 
     LearningHandle = figure;
     for epoch = 1:num_epochs
-        if mod(epoch, 100)==0; disp(['Epoch: ',num2str(epoch)]); end
+        P=randperm(N);
+        for b =1:minibatch_size:N;
+            % Pick a minibatch sample
+            X1 = X(:,P((b+1):min((b+minibatch_size),N)))
+            Y = softmax(W * X1);             % Compute activations
+            E = T - Y;                       
+            gradLoss = -E * X1' + alpha * W;;
+            W = W - eta * gradLoss;
+            G = loss(W,Y,T,alpha);          % Test on the original sample
+            Gn = [Gn,G];
 
-        % Update weights
-        W_old = W;
-        W = W - eta * gradLoss;
+            %  Limit the history to 100
+            if length(Gn) == 101
+                Gn = Gn(2:101);
+            end
 
-        %% Update gradient
-        gradLoss_old = gradLoss;
-        Y = softmax(W * X);          % Compute activations
-        E = T - Y;
-        gradLoss = -E * X' + alpha * W;
-
-        G = loss(W,Y,T,alpha);          % Test on the original sample
-        Gn = [Gn,G];
-
-        % Adjust learning rate according to Barzilai-Borwein
-
-        eta = ((W(:) - W_old(:))' * (gradLoss(:) - gradLoss_old(:))) ...
-              ./ (eps + norm(gradLoss(:) - gradLoss_old(:))^2 );
-
-        %  Limit the history to 100
-        if length(Gn) == 101
-            Gn = Gn(2:101);
+            % Visualize  learning
+            if mod(epoch, 10) == 0 
+                set(0, 'CurrentFigure', LearningHandle),
+                plot(Gn,'-o'), 
+                title(['Learning (epoch: ',num2str(epoch),')']),
+                disp(['Learning rate: ',num2str(eta)]);
+                drawnow;
+            end
+            % Re-center the weights
+            if mod(epoch, 100) == 0 
+                W = W - mean(W,1);
+            end;
         end
-
-        if eta < min_eta
-            disp('Learning rate threshold met, stopping...');        
-            break;
-        end
-
-
-        % Visualize  learning
-        if mod(epoch, 10) == 0 
-            set(0, 'CurrentFigure', LearningHandle),
-            plot(Gn,'-o'), 
-            title(['Learning (epoch: ',num2str(epoch),')']),
-            disp(['Learning rate: ',num2str(eta)]);
-            drawnow;
-        end
-        % Re-center the weights
-        if mod(epoch, 100) == 0 
-            W = W - mean(W,1);
-        end;
-        %pause(.1);
     end
 
     NErrors = length(find(round(Y)~=T));
