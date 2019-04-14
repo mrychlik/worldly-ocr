937b49ec789f2644990bd5657324ce140afa66d3
Marek Rychlik <marek@marekrychlik.com>
2019-04-14 09:37:31 -0700

    *** empty log message ***

--- LogisticRegression/my_script.m
+++ LogisticRegression/my_script.m
@@ -5,17 +5,13 @@ digit3 = 3;
 [X,T] = prepare_training_data(digit0,digit1,digit2,digit3);
 % Matlab expects samples in columns and T to be a row vector
 
-% Change pixel value to logarithmic odds off being black
-%X = log ( (.1 + X) ./ (1 + .1 - X) );
-
-
-% Insert a column of 1's
-X = [ones([size(X,1),1]),X];
+% Insert a row of 1's
+X = [ones([1,size(X,2)]);X];
 
 % Straight from PATTERNNET help page
-num_epochs = 5000;
+num_epochs = 10000;
 [Y, NErrors] = train_patternnet(X,T,num_epochs);
 
 figure;
-plotconfusion(T',Y');
+plotconfusion(T,Y);
 NErrors
