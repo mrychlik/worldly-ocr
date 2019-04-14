f8994a850d0d04e035cf9d09ad0f24b51f27f921
Marek Rychlik <marek@marekrychlik.com>
2019-04-14 10:33:31 -0700

    *** empty log message ***

--- ChineseCharHarvesting/my_script.m
+++ ChineseCharHarvesting/my_script.m
@@ -7,7 +7,7 @@ X = [ones([1,size(X,2)]);X];
 
 % Straight from PATTERNNET help page
 num_epochs = 100000;
-minibatch_size = 2;
+minibatch_size = 16;
 [Y, NErrors, W] = train_patternnet(X,T,num_epochs,minibatch_size);
 
 NErrors
