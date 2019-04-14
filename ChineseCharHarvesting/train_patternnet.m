347b3a3d58881e7b6611f95bac72f1974aed48f0
Marek Rychlik <marek@marekrychlik.com>
2019-04-14 16:00:23 -0700

    *** empty log message ***

--- ChineseCharHarvesting/train_patternnet.m
+++ ChineseCharHarvesting/train_patternnet.m
@@ -4,7 +4,7 @@ function [Y,NErrors,W] = train_patternnet(X, T, num_epochs, minibatch_size)
     if nargin < 3; num_epochs = 10000; end
     if nargin < 4; minibatch_size = 128; end
 
-    eta = 1e-2;                         % Fixed learning rate
+    eta = 2e-2;                         % Fixed learning rate
     alpha = 1e-1;                       % Regularizer constant
 
     assert(size(X,2) == size(T,2), ['Inconsistent number of samples in ' ...
