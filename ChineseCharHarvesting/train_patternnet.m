334bd623416aae7ddd001465053337728b4d5714
Marek Rychlik <marek@marekrychlik.com>
2019-04-13 23:46:03 -0700

    *** empty log message ***

--- ChineseCharHarvesting/train_patternnet.m
+++ ChineseCharHarvesting/train_patternnet.m
@@ -1,5 +1,5 @@
 function [Y,NErrors,W] = train_patternnet(X, T, num_epochs, minibatch_size)
-% NERRORS = TRAIN_PATTERNNET(X, T, NUM_EPOCHS, MINIBATCH_SIZE) trains
+% [Y,NERRORS,W] = TRAIN_PATTERNNET(X, T, NUM_EPOCHS, MINIBATCH_SIZE) trains
 % a pattennet with H hidden neurons.
     if nargin < 3; num_epochs = 128; end
     if nargin < 4; minibatch_size = 64; end
