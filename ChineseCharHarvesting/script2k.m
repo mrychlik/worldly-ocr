4c6b58bc1f533dd94af63d045bd3d8e683803e4a
Marek Rychlik <marek@marekrychlik.com>
2019-04-14 20:32:24 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/script2k.m
@@ -0,0 +1,23 @@
+[X,T] = prepare_training_data;
+
+% Matlab expects samples in columns and T to be a row vector
+
+% Insert a row of 1's
+X = [ones([1,size(X,2)]);X];
+
+% Pick a subsample
+N=2000;
+X=X(:,1:N);
+T=T(:,1:N);
+
+
+% Straight from PATTERNNET help page
+num_epochs = 100000;
+minibatch_size = 256;
+[Y, NErrors, W] = train_patternnet(X,T,num_epochs,minibatch_size);
+
+for j=1:10
+    [Y, NErrors, W] = train_patternnet(X,T,num_epochs,minibatch_size,W);
+end
+
+NErrors
