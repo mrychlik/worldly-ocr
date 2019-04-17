0de5cb751ec02480b395d93bfa9ea1fa0dce48f1
Marek Rychlik <marek@marekrychlik.com>
2019-04-16 20:51:59 -0700

    *** empty log message ***

--- ChineseCharHarvesting/recognize.sh
+++ ChineseCharHarvesting/recognize.sh
@@ -30,6 +30,6 @@ mkdir -p $OUT
 
 for f in $DATADIR/*
 do
-    tesseract --oem1 -l $LANG --psm 10 $f ${f%%.pbm}
+    tesseract --oem 1 -l $LANG --psm 10 $f ${f%%.pbm}
     mv ${f%%.pbm}.txt $OUT
 done
