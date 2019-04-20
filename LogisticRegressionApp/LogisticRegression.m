141565f3608bfcdde3115329dd5b5c407b3e4b5c
Marek Rychlik <marek@marekrychlik.com>
2019-04-20 01:31:15 -0700

    *** empty log message ***

--- LogisticRegressionApp/LogisticRegression.m
+++ LogisticRegressionApp/LogisticRegression.m
@@ -277,7 +277,8 @@ classdef LogisticRegression
             % Find indices which label is correct
             idx = find(this.T(digit_idx,:));
             mean_digit = reshape(mean(this.X(:,idx),2), [this.Height,this.Width])'; 
-            this.ImageHandle.CData  = 1 - mean_digit .* this.app.hint_intensity;
+            this.ImageHandle.CData  = this.app.hint_intensity + ...
+                mean_digit .* (1-this.app.hint_intensity);
             colormap(this.app.UIAxes2,gray);
             drawnow;
         end
