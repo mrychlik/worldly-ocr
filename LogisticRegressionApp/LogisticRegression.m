08004a5d18e154ae9efd0b9a1f56de6d209fdd52
Marek Rychlik <marek@marekrychlik.com>
2019-04-19 23:21:19 -0700

    *** empty log message ***

--- LogisticRegressionApp/LogisticRegression.m
+++ LogisticRegressionApp/LogisticRegression.m
@@ -274,7 +274,7 @@ classdef LogisticRegression
         end
 
         function this = clear_digit(this)
-            this.ImageHandle = image(this.app.UIAxes2, zeros(this.Height,this.Width));
+            this.ImageHandle = imshow(this.app.UIAxes2, zeros(this.Height,this.Width));
             colormap(this.app.UIAxes2,gray);
         end
 
