1294d3568c73780c604f9887b9fcb83429b211d1
Marek Rychlik <marek@marekrychlik.com>
2019-04-24 19:23:16 -0700

    *** empty log message ***

--- LogisticRegressionApp/LogisticRegression.m
+++ LogisticRegressionApp/LogisticRegression.m
@@ -211,7 +211,7 @@ classdef LogisticRegression
             data_file = fullfile(this.app_data_path, 'digit_data.mat');
             load(data_file);
 
-            this.show_digits;
+            this.show_digits(I);
 
             % Height and width of images
             this.Height = size(Digit{1},2);
@@ -242,7 +242,7 @@ classdef LogisticRegression
             this.T = T0(P,:)';
         end
 
-        function this = show_digits(this)
+        function this = show_digits(this, I)
             digits = this.app.digits;
             num_digits = length(digits);
             this.app.DigitViewerPanel.AutoResizeChildren = 'off';
