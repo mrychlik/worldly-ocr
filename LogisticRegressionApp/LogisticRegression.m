addab9a79ea1896a872f97761781c00970e0d748
Marek Rychlik <marek@marekrychlik.com>
2019-04-25 07:23:10 -0700

    *** empty log message ***

--- LogisticRegressionApp/LogisticRegression.m
+++ LogisticRegressionApp/LogisticRegression.m
@@ -407,6 +407,7 @@ classdef LogisticRegression
                 saved_state.losses = this.losses;
                 saved_state.eta = this.eta;
                 saved_state.NErrors = this.NErrors;
+                saved_state.Y = this.Y;
 
                 % Write the file
                 save(fullfile(path,file), 'saved_state');
@@ -428,10 +429,11 @@ classdef LogisticRegression
                 this.losses = saved_state.losses;
                 this.eta = saved_state.eta;
                 this.NErrors = saved_state.NErrors;
+                this.Y = saved_state.Y;
                 
                 % Update learning state display
                 this = this.show_learning;
-                this = this.plot_confusion;
+                this.plot_confusion;
             end
         end
 
