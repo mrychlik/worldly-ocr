26451396e93dc7f3d477e5289b5d55a462099eb5
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 16:01:28 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/recognize_all.m
@@ -0,0 +1,16 @@
+datadir='BlackOnWhiteChars'
+out='OutputsAsUTF8'
+image_files = dir(fullfile(datadir,'*.pbm'));
+num_files = numel(image_files);
+
+for f=1:num_files
+    fname=image_files(f).name;
+    fbase=fname(1:end-4);
+    fpath = fullfile(datadir, fname);
+    BW = imread(fpath);
+    chi_str=recognize(BW);
+    txt_fname=fullfile(out,[fbase,'.txt']);
+    fh = fopen(txt_fname,'wb+');
+    fwrite(fh,chi_str,'char');
+    fclose(fh);
+end
