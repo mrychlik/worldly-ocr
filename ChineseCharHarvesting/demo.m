da0d20de2700617603aad8f82a6e9dcf30cc6e22
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 10:54:59 -0700

    *** empty log message ***

--- ChineseCharHarvesting/demo.m
+++ ChineseCharHarvesting/demo.m
@@ -18,15 +18,17 @@ end
 function chi_text(str)
 % Draw the characters
     font='Arial';
-    fontsize=144;
+    fontsize=300;
+    hold on;
     s=text(0.5, 0.5, str, ...
            'FontSize', fontsize,...
-           'FontName',font)
-    set(s,'Units', 'normalized');
+           'FontName',font,...
+           'Units', 'pixels',...
+           'FontUnits','Pixels');
     r=s.Extent;
     q=rectangle('Position',[r(1),r(2),r(3)-r(1),r(4)-r(2)],'LineWidth',3);
     m=0.001;                            % Margin
-    m=0;                            % Margin    
     xlim([r(1)-m,r(3)+m]);
     ylim([r(2)-m,r(4)+m]);
+    hold off;
 end
