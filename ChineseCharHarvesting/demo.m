fc22c5bdb4b381b11bbafc77fc5e8d207b49edcd
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 08:35:48 -0700

    *** empty log message ***

--- ChineseCharHarvesting/demo.m
+++ ChineseCharHarvesting/demo.m
@@ -3,7 +3,7 @@ N=size(X,2);
 X=reshape(X,[max_h,max_w,N]);
 for k=1:N; 
     imagesc(X(:,:,k));
-    title(sprintf('Character %d',k));
+    title(sprintf('Character %d, label: %s',k,C{k}));
     drawnow;
     pause(0.05); 
 end
