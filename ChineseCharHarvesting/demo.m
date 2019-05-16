6bab3dd84e521a2614fcd230966d93412b05a098
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 08:45:34 -0700

    *** empty log message ***

--- ChineseCharHarvesting/demo.m
+++ ChineseCharHarvesting/demo.m
@@ -2,8 +2,26 @@ load('training_data');
 N=size(X,2);
 X=reshape(X,[max_h,max_w,N]);
 for k=1:N; 
+    clf;
+    subplot(1,2,1);
     imagesc(X(:,:,k));
-    title(sprintf('Character %d, label: %s',k,C{k}));
+    title(sprintf('Character %d',k));
+    subplot(1,2,2);
+    title(sprintf('Label: %s',C{k}));
+    chi_text(C{k});
     drawnow;
-    pause(0.05); 
+    pause(0.5); 
+end
+
+
+function chi_text(str)
+% Draw the characters
+    font='Arial';
+    fontsize=72;
+    s=text(0, 0, str, 'FontSize', fontsize'FontName',font,'FontUnits','Pixels');
+    r=s.Extent;
+    q=rectangle('Position',[r(1),r(2),r(3)-r(1),r(4)-r(2)],'LineWidth',1);
+    m=0.02;                                 % Margin
+    xlim([r(1)-m,r(3)+m]);
+    ylim([r(2)-m,r(4)+m]);
 end
