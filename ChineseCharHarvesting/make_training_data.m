c0bc425ac10c0b2e4c22eda6d702181df787b598
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 14:01:05 -0700

    *** empty log message ***

--- ChineseCharHarvesting/make_training_data.m
+++ ChineseCharHarvesting/make_training_data.m
@@ -57,6 +57,7 @@ for char_count=1:N
     [fid, msg]=fopen(txtfile,'r');
     if ~isempty(msg)
         error(sprintf('Could not open file %s: message: %s',txtfile, msg));
+        str{char_count}='BAD';
     end
     [bytes,count]=fread(fid,'uint8');
     str{char_count}=native2unicode(bytes');
