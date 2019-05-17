c1791e1e13638cf63a11a91e1546b22ef855d878
Marek Rychlik <marek@marekrychlik.com>
2019-05-16 23:37:16 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/play_weights_2k.m
@@ -0,0 +1,11 @@
+%
+% Some Fun:
+% Demonstrate that the learnt weights contain
+% Chinese characters
+%
+[~,~,Height,Width] = prepare_training_data;
+load('best_weights_2k');
+for r=1:size(W,1)
+    imagesc(reshape(squeeze(W(r,2:end)),[Height,Width]));
+    drawnow;pause(1);
+end
