a406caa776ea8258be946632cea7ce567d5626eb
Marek Rychlik <marek@marekrychlik.com>
2019-06-07 09:47:19 -0700

    *** empty log message ***

--- AzuAckuRisalah/script4.m
+++ AzuAckuRisalah/script4.m
@@ -3,7 +3,7 @@
 imgfname = fullfile('images','LineOfPashto.png');
 BW = imread(imgfname);
 psm = 7;
-language = 'pus';                       % Pashto
-% language = 'fas';                       % Persian
+%language = 'pus';                       % Pashto
+language = 'fas';                       % Persian
 r = TesseractRecognizer(psm,language);
 [str,status] = r.recognize(BW);
