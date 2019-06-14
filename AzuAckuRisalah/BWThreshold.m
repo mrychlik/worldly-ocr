5c700d45954a9a12e4249261f26f3bbf3c4fba38
Marek Rychlik <marek@marekrychlik.com>
2019-06-14 12:00:25 -0700

    *** empty log message ***

--- AzuAckuRisalah/BWThreshold.m
+++ AzuAckuRisalah/BWThreshold.m
@@ -1,7 +1,9 @@
 function [BW] = BWThreshold(I,Type)
-% This is function to use change a color image to grayscale first, then
+% BWTHRESHOLD changes a color image to monochrome.
+%  [BW] = BWTHRESHOLD(I,TYPE) changes a color image I to monochrome,
+%  according to an integer TYPE grayscale first, then
 %  using different methods based on Type, it changes the figure I to
-%  grayscale and then to Balck and White
+%  grayscale and then to black and white.
     Igray=rgb2gray(I);
     switch Type
       case 1 %Threshold is fixed at 0.2
@@ -11,7 +13,31 @@ function [BW] = BWThreshold(I,Type)
       case 3 %Adaptive Filter using Mean Method
         T=adaptthresh(Igray,0.2,'ForegroundPolarity','dark','Statistic','mean');
       case 4 %Adaptive Filter using Gaussian Method
-        T=adaptthresh(Igray,0.2,'ForegroundPolarity','dark','Statistic','gaussian');
+        T=adaptthresh(Igray,0.2,'ForegroundPolarity','dark','Statistic','gaussian');dirpath=fullfile('Pages');
+imgname='page-01.ppm';
+%imgname='page-06.ppm';
+% imgname='page-07.ppm';
+% imgname='page-08.ppm';
+% imgname='page-09.ppm';
+% imgname='page-10.ppm';
+% imgname='page-11.ppm';
+% imgname='page-12.ppm';
+% imgname='page-13.ppm';
+% imgname='page-14.ppm';
+% imgname='page-15.ppm';
+% imgname='page-16.ppm';
+
+imgfile=fullfile(dirpath,imgname)
+I=imread(imgfile);
+imshow(I);
+
+%BW=~im2bw(I,.2);
+
+method=2;                               % Same as threshold=.2
+BW=BWThreshold(I,method);
+
+imshow(BW);
+
       case 5 %Adaptive Filter using Median Method
              % warning(msg)
         T=adaptthresh(Igray,0.2,'ForegroundPolarity','dark','Statistic','median');
