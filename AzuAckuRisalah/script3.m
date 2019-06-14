b677e120cf802028927db6a0c266b56d63ef186f
Marek Rychlik <marek@marekrychlik.com>
2019-06-14 13:53:59 -0700

    *** empty log message ***

--- AzuAckuRisalah/script3.m
+++ AzuAckuRisalah/script3.m
@@ -1,4 +1,5 @@
 dirpath=fullfile('Pages');
+imgname='page-01.ppm';
 %imgname='page-06.ppm';
 % imgname='page-07.ppm';
 % imgname='page-08.ppm';
@@ -7,7 +8,7 @@ dirpath=fullfile('Pages');
 % imgname='page-11.ppm';
 % imgname='page-12.ppm';
 % imgname='page-13.ppm';
-imgname='page-14.ppm';
+% imgname='page-14.ppm';
 % imgname='page-15.ppm';
 % imgname='page-16.ppm';
 
@@ -17,8 +18,40 @@ imshow(I);
 
 %BW=~im2bw(I,.2);
 
-method=1;                               % Same as threshold=.2
+method=3;                               % Same as threshold=.2
 BW=BWThreshold(I,method);
+
+imshow(BW);
+
+obj = LineBreaker(BW);
+
+obj.show_labels();
+pause(3);
+obj.plot_lines();
+pause(2);dirpath=fullfile('Pages');
+imgname='page-01.ppm';
+%imgname='page-06.ppm';
+% imgname='page-07.ppm';
+% imgname='page-08.ppm';
+% imgname='page-09.ppm';
+% imgname='page-10.ppm';
+% imgname='page-11.ppm';
+% imgname='page-12.ppm';
+% imgname='page-13.ppm';
+% imgname='page-14.ppm';
+% imgname='page-15.ppm';
+% imgname='page-16.ppm';
+
+imgfile=fullfile(dirpath,imgname)
+I=imread(imgfile);
+imshow(I);
+
+%BW=~im2bw(I,.2);
+
+method=3;                               % Same as threshold=.2
+BW=BWThreshold(I,method);
+
+imshow(BW);
 
 obj = LineBreaker(BW);
 
@@ -26,6 +59,24 @@ obj.show_labels();
 pause(3);
 obj.plot_lines();
 pause(2);
+
+
+% Short lines could be diacriticals
+obj.show_short_lines(.2);
+drawnow;
+
+% Set parameter to absorb diacriticals
+obj.SigmaFactor=1;                      % To be experimentally determined
+
+obj=merge_short_lines(obj);
+
+% After merging diacriticals should be in their rightful places
+obj.show_short_lines;
+
+
+obj.play_lines(3);
+
+
 
 
 % Short lines could be diacriticals
