0a9bbb83d549a0891c057b9282dc855ba140c1ff
Marek Rychlik <marek@marekrychlik.com>
2019-06-15 09:50:28 -0700

    *** empty log message ***

--- AzuAckuRisalah/script3.m
+++ AzuAckuRisalah/script3.m
@@ -30,36 +30,6 @@ obj = LineBreaker(BW);
 obj.show_labels();
 pause(3);
 obj.plot_lines();
-pause(2);dirpath=fullfile('Pages');
-imgname='page-01.ppm';
-%imgname='page-06.ppm';
-% imgname='page-07.ppm';
-% imgname='page-08.ppm';
-% imgname='page-09.ppm';
-% imgname='page-10.ppm';
-% imgname='page-11.ppm';
-% imgname='page-12.ppm';
-% imgname='page-13.ppm';
-% imgname='page-14.ppm';
-% imgname='page-15.ppm';
-% imgname='page-16.ppm';
-
-imgfile=fullfile(dirpath,imgname)
-I=imread(imgfile);
-imshow(I);
-
-%BW=~im2bw(I,.2);
-
-method=3;                               % Same as threshold=.2
-BW=BWThreshold(I,method);
-
-imshow(BW);
-
-obj = LineBreaker(BW);
-
-obj.show_labels();
-pause(3);
-obj.plot_lines();
 pause(2);
 
 
@@ -77,9 +47,6 @@ obj.show_short_lines;
 
 
 obj.play_lines(3);
-
-
-
 
 % Short lines could be diacriticals
 obj.show_short_lines(.2);
