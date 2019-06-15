bdf6a20d55200cb8ec39b889e7db9a6155464951
Marek Rychlik <marek@marekrychlik.com>
2019-06-15 09:44:55 -0700

    *** empty log message ***

--- AzuAckuRisalah/script3.m
+++ AzuAckuRisalah/script3.m
@@ -1,6 +1,7 @@
 dirpath=fullfile('Pages');
-imgname='page-01.ppm';
-%imgname='page-06.ppm';
+% imgname='page-01.ppm';
+imgname='page-02.ppm';
+% imgname='page-06.ppm';
 % imgname='page-07.ppm';
 % imgname='page-08.ppm';
 % imgname='page-09.ppm';
