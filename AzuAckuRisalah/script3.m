2e6cd369e63aa2c986b369a32327118fe7f6c14b
Sayyed Mohsen Vazirizade <s.m.vazirizade@gmail.com>
2019-06-19 15:46:17 -0700

    Update script3.m

--- AzuAckuRisalah/script3.m
+++ AzuAckuRisalah/script3.m
@@ -8,12 +8,10 @@
 %%
 
 %% Choose the image to be processed. The processing includes:
-
-dirpath=fullfile('Pages');
-% imgname='page-01.ppm';
-% imgname='page-02.ppm';
-% imgname='page-03.ppm';
-imgname='page-06.ppm';
+% Address of the folder the image is located
+dirpath='Pages';
+% Name of the image
+%imgname='page-06.ppm';
 % imgname='page-07.ppm';
 % imgname='page-08.ppm';
 % imgname='page-09.ppm';
@@ -21,27 +19,17 @@ imgname='page-06.ppm';
 % imgname='page-11.ppm';
 % imgname='page-12.ppm';
 % imgname='page-13.ppm';
-% imgname='page-14.ppm';
+imgname='page-14.ppm';
 % imgname='page-15.ppm';
 % imgname='page-16.ppm';
 
-%% Read the image in
-
-imgfile=fullfile(dirpath,imgname)
+%% reading the figure according to the address and defining the lines
+imgfile=fullfile(dirpath,imgname);
 I=imread(imgfile);
 imshow(I);
-
-%BW=~im2bw(I,.2);
-
-%% Binarize the image
-
-method=3;                               % Same as threshold=.2
-BW=BWThreshold(I,method);
-
-imshow(BW);
-
-%% Break up the image into lines
-
+pause(1);
+%BW=~im2bw(I,.2); 
+BW=BWThreshold(I,1);
 obj = LineBreaker(BW);
 
 obj.show_labels();
@@ -50,26 +38,22 @@ obj.plot_lines();
 pause(2);
 
 
-%% Show lines suspiciously short. Short lines could be diacriticals
-
+%%
+% Short lines could be diacriticals
 obj.show_short_lines(.2);
 drawnow;
 
-%% Attempt to adjoint diacriticals to correct lines
-
 % Set parameter to absorb diacriticals
 obj.SigmaFactor=1;                      % To be experimentally determined
-
 obj=merge_short_lines(obj);
 
 % After merging diacriticals should be in their rightful places
 obj.show_short_lines;
 
-%% Show all lines after attaching diacriticals
+
 obj.play_lines(3);
 
-%% Show remaining short lines.
-%obj.show_short_lines(.2);
-
-
-
+for label=1:max(obj.LabeledLines)
+    imshow(obj.LabeledLines==label); 
+    pause(1); 
+end
