2b5d313bff0b9fc863561c594a527dc72e3e4ac3
smvazirizade <s.m.vazirizade@gmail.com>
2019-06-26 11:36:39 -0700

    Update script4.m

--- AzuAckuRisalah/script4.m
+++ AzuAckuRisalah/script4.m
@@ -1,10 +1,32 @@
 % Tests TesseractRecognizer
 % Performs OCR on a line of text.
 %imgfname = fullfile('images','LineOfPashto.png');
-imgfname = fullfile('imagesamples','image001.png');
+clc;clear
+dirpathsave='imagesamples';     %direction for saving each line
+for lablel=1:1
+name1=sprintf('image%03.0f.png',lablel);
+imgfname = fullfile(dirpathsave,name1);
 BW = imread(imgfname);
+imshow(BW);
 psm = 7;
-%language = 'pus';                       % Pashto
+language = 'pus';                       % Pashto
+r = TesseractRecognizer(psm,language);
+[str1,status1] = r.recognize(BW);
+
 language = 'fas';                       % Persian
 r = TesseractRecognizer(psm,language);
-[str,status] = r.recognize(BW);
+[str2,status2] = r.recognize(BW);
+
+Difference=str1-str2;
+sum(Difference)
+if sum(Difference)~=0 
+    warning("pus and fas are not the same!")
+end
+
+
+
+name2=sprintf('image%03.0f.txt',lablel);
+fulname = fullfile(dirpathsave,name2);
+dlmwrite(str1,fulname); 
+
+end
