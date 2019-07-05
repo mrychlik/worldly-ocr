741c91076c6574922fabf91c70400812606f2ac5
Marek Rychlik <marek@marekrychlik.com>
2019-07-05 11:30:55 -0700

    *** empty log message ***

--- ChineseCharHarvesting/scanner.m
+++ ChineseCharHarvesting/scanner.m
@@ -99,34 +99,34 @@ function sorted=sort_stats(stats)
 end
 
 
-function [BW] = binarize(I,Type,Thres)
+function [BW] = binarize(I,Type,Threshold)
 % BWTHRESHOLD changes a color image to monochrome.
-%  [BW] = BWTHRESHOLD(I,TYPE,Thres) changes a color image I to monochrome,
+%  [BW] = BWTHRESHOLD(I,TYPE,THRESHOLD) changes a color image I to monochrome,
 %  according to an integer TYPE grayscale first, then
 %  using different methods based on Type, it changes the figure I to
 %  grayscale and then to black and white.
-%  Thres is defining the the threshold value; it is optional and the
+%  THRESHOLD is defining the the threshold value; it is optional and the
 %  default is 0.2
-if  nargin < 3
-    Thres=0.2;
-end
-Igray=rgb2gray(I);
- switch Type
-    case 1 %Threshold is fixed at 0.2
-        T=Thres;
-    case 2 %Threshold is global using Otsu Method
+    if  nargin < 3
+        Threshold=0.2;
+    end
+    Igray=rgb2gray(I);
+    switch Type
+      case 1 %Thresholdhold is fixed at 0.2
+        T=Threshold;
+      case 2 %Thresholdhold is global using Otsu Method
         T=('global');
         if  nargin == 3
-            warning("Thres parameter is not being used in Otsu Method!")
+            warning("Threshold parameter is not being used in Otsu Method!")
         end
-    case 3 %Adaptive Filter using Mean Method
-        T=adaptthresh(Igray,Thres,'ForegroundPolarity','dark','Statistic','mean');
-    case 4 %Adaptive Filter using Gaussian Method
-        T=adaptthresh(Igray,Thres,'ForegroundPolarity','dark','Statistic','gaussian');
-    case 5 %Adaptive Filter using Median Method
-      warning("Using Mean Method might be very time comsuming!")
-      T=adaptthresh(Igray,Thres,'ForegroundPolarity','dark','Statistic','median');
-end
-BW=~imbinarize(Igray,T);
+      case 3 %Adaptive Filter using Mean Method
+        T=adaptthresh(Igray,Threshold,'ForegroundPolarity','dark','Statistic','mean');
+      case 4 %Adaptive Filter using Gaussian Method
+        T=adaptthresh(Igray,Threshold,'ForegroundPolarity','dark','Statistic','gaussian');
+      case 5 %Adaptive Filter using Median Method
+        warning("Using Mean Method might be very time comsuming!")
+        T=adaptthresh(Igray,Threshold,'ForegroundPolarity','dark','Statistic','median');
+    end
+    BW=~imbinarize(Igray,T);
 end
 
