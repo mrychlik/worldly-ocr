ac80ef690e445aee9af473a244d9812fbfa7814f
Marek Rychlik <marek@marekrychlik.com>
2019-07-05 09:30:57 -0700

    *** empty log message ***

--- LogisticRegression/my_script.m
+++ LogisticRegression/my_script.m
@@ -5,8 +5,17 @@ digit3 = 3;
 [X,T] = prepare_training_data(digit0,digit1,digit2,digit3);
 % Matlab expects samples in columns and T to be a row vector
 
+
 % Insert a row of 1's
 X = [ones([1,size(X,2)]);X];
+
+% Regularize T
+[D,N] = size(X);
+[C,~]=size(T);
+
+T0=1/C*ones(C,1)*ones(1,N);
+t=.99;
+T=(1-t)*T0+t*T;
 
 % Straight from PATTERNNET help page
 num_epochs = 5000;
