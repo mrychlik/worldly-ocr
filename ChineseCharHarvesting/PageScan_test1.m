dd06f7ae304c944341a62ee8e1ab34d93d864845
Marek Rychlik <marek@marekrychlik.com>
2019-07-05 21:54:34 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/PageScan_test1.m
@@ -0,0 +1,11 @@
+pagedir='Pages';
+page_img_pattern='page-%02d.ppm';
+
+for page=6:96
+    filename=fullfile(pagedir,sprintf(page_img_pattern,page));
+    ps = PageScan;
+    ps = ps.scanfile(filename);
+    ps.marked_page_img;
+    drawnow;
+    pause(2);
+end;
