cdb902c25ca5c7b1e0e65deb0a56a2d3d8d7753a
Marek Rychlik <marek@marekrychlik.com>
2019-07-07 11:12:30 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test1.m
+++ ChineseCharHarvesting/PageScan_test1.m
@@ -4,12 +4,13 @@ page_img_pattern='page-%02d.ppm';
 %bg='Mono';
 %bg='Foo';                               % Invalid option - test
 bg='Original';
+show_dilation = true;
 
 for page=6:95
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan;
     ps = ps.scanfile(filename);
-    ps.show_marked_page_img(bg);
+    ps.show_marked_page_img('Background',bg,'ShowDilation',show_dilation);
     drawnow;
     pause;
 end;
