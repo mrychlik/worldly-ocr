28cb39132d9947704a1162714d046fab0014e741
Marek Rychlik <marek@marekrychlik.com>
2019-07-07 08:27:23 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test1.m
+++ ChineseCharHarvesting/PageScan_test1.m
@@ -5,11 +5,12 @@ page_img_pattern='page-%02d.ppm';
 %bg='Foo';                               % Invalid option - test
 bg='Original';
 
-for page=6:95
+%for page=6:95
+for page=6
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan;
     ps = ps.scanfile(filename);
-    ps.marked_page_img(bg);
+    ps.show_marked_page_img(bg);
     drawnow;
     pause;
 end;
