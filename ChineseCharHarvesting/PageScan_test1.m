26c2975cc467ba467bbcdcff52c77de1520c7a66
Marek Rychlik <marek@marekrychlik.com>
2019-07-08 08:12:23 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test1.m
+++ ChineseCharHarvesting/PageScan_test1.m
@@ -5,13 +5,16 @@ page_img_pattern='page-%02d.ppm';
 %bg='Foo';                               % Invalid option - test
 bg='Original';
 show_dilation = true;
+show_outliers = true;
 
 %for page=6:95
-for page=6
+for page=6:10
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan;
     ps = ps.scanfile(filename);
-    ps.show_marked_page_img('Background',bg,'ShowDilation',show_dilation);
+    ps.show_marked_page_img('Background',bg,...
+                            'ShowDilation',show_dilation,...
+                            'ShowOutliers',show_outliers);
     title(sprintf('Page %d', page));
     drawnow;
     uiwait(gcf);
