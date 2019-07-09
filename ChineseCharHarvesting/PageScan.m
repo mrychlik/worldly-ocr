df47926911e8daaefc6b9f6e25871e463ce3cd14
Marek Rychlik <marek@marekrychlik.com>
2019-07-09 15:17:46 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -487,6 +487,10 @@ classdef PageScan
         end
 
         function [side,x] = BindingSide(this) 
+        % BINDINGSIDE - returns the side of the binding
+        %  [SIDE,X] = BINDINGSIDE(THIS) returns the 
+        %  side of the page which is adjacent to the binding.
+        %  The result can be 'Left', 'Right', or [].
             [T,R] = this.VerticalLines;
             % The equation of the line is R=cos(T)*x+sin(T)*y
             % where T is small, thus cos(T)~=0. Hence, x = (R-sin(T)*y)/cos(T)
