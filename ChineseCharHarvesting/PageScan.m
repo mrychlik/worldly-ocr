db99e3f65185b580be91e186686c840daa87324e
Marek Rychlik <marek@marekrychlik.com>
2019-07-09 15:35:39 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -45,7 +45,7 @@ classdef PageScan
         Boundary;                       % Page boundary
         HorizontalBoundary;             % Top and bottom of page
         % VerticalBoundary              % Currently function, as we want to pass some options
-        BindingSide;
+        Binding;                        % Information about book binding
     end
 
     methods
