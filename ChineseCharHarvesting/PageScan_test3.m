a4ddca4afeacf676119c8f3669f5eedc1bb22bbb
Marek Rychlik <marek@marekrychlik.com>
2019-07-10 12:46:11 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test3.m
+++ ChineseCharHarvesting/PageScan_test3.m
@@ -1,7 +1,8 @@
 pagedir='Pages';
 page_img_pattern='page-%02d.ppm';
+if ~exist('pages','var') pages=6:95; end;
 
-for page=6:95
+for page=pages
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan(filename);
     ps.show_centroids;
