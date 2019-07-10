fd63c3bbf230cc9767159696ef3d3b48bc6b5156
Marek Rychlik <marek@marekrychlik.com>
2019-07-10 12:45:05 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test6.m
+++ ChineseCharHarvesting/PageScan_test6.m
@@ -1,12 +1,15 @@
 pagedir='Pages';
 page_img_pattern='page-%02d.ppm';
+if ~exist('pages','var') pages=6:95; end;
+
 show_outliers=false;
-keep_outliers=true;
+keep_outliers=false;
+show_bboxes=true;
 
 for page=6:95
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan(filename,'KeepOutliers',keep_outliers);
-    ps.show_column_centers;
+    ps.show_column_centers('ShowOutliers',show_outliers, 'ShowBoundingBoxes',show_bboxes);
     title(sprintf('Page %d', page));
     drawnow;
     uiwait(gcf);
