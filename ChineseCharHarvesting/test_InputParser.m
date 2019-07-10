754b938cb87224bf297812904f6057b0a82ccb6e
Marek Rychlik <marek@marekrychlik.com>
2019-07-10 12:40:27 -0700

    *** empty log message ***

--- ChineseCharHarvesting/test_InputParser.m
+++ ChineseCharHarvesting/test_InputParser.m
@@ -1,18 +1,21 @@
-make_parser;
+function test_InputParser
+p=make_parser;
 
 varargin1 = {'a',7,'c',false,'b',17,'d',[]};
 parse(p, 'a',varargin1{:});
 p.Results
 
+p=make_parser;
+
 varargin2 = {'a',7,'b',15,'c',23,'d',[]};
 parse(p, 'a',varargin2{:});
 p.Results
-
+end
 
 function p = make_parser
-p = inputParser;
-addRequired(p, 'a', @(x)isscalar(x));
-addParameter(p, 'd', [], @(x)isempty(x));
-addOptional(p, 'b', true, @(x)isscalar(x));
-addOptional(p, 'c', true, @(x)isscalar(x));
+    p = inputParser;
+    addRequired(p, 'a', @(x)isscalar(x));
+    addParameter(p, 'd', [], @(x)isempty(x));
+    addOptional(p, 'b', true, @(x)isscalar(x));
+    addOptional(p, 'c', true, @(x)isscalar(x));
 end
