a2c4c59bbd7a5a212e0eec92751e91ee746512ad
Marek Rychlik <marek@marekrychlik.com>
2019-07-11 11:20:14 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test11.m
+++ ChineseCharHarvesting/PageScan_test11.m
@@ -8,7 +8,7 @@ keep_outliers=false;
 for page=pages
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan(filename,'KeepOutliers',keep_outliers);
-    ps.identify_merge_characters;
+    ps.show_merge_characters;
     %title(sprintf('Page %d', page));
     drawnow;
     uiwait(gcf);
