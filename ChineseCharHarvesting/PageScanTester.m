2fe48f5ecbd0637cbbf623589bdb8afe75429c48
Marek Rychlik <marek@marekrychlik.com>
2019-07-13 10:07:08 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScanTester.m
+++ ChineseCharHarvesting/PageScanTester.m
@@ -1,6 +1,6 @@
 classdef PageScanTester
 
-    properties(Constant) 
+    properties(Constant, Access=private) 
         DefaultPageDir = 'Pages';
         DefaultPageImgPattern = 'page-%02d.ppm';
         DefaultPages = 6:95;
@@ -22,11 +22,33 @@ classdef PageScanTester
             addOptional(p, 'PageImgPattern', this.DefaultPageImgPattern, @(x)ischar(x));
             addOptional(p, 'Pages', this.DefaultPages, @(x)isnumeric(x));
 
-            parse(p, this,varargin{:});
+            parse(p, varargin{:});
 
             this.PageDir = p.Results.PageDir;
             this.PageImgPattern = p.Results.PageImgPattern;
             this.Pages = p.Results.Pages;
+        end
+
+        function run(this)
+            this.test1;
+        end
+
+        function test1(this)
+            disp(mfilename);
+            bg='Original';
+            show_dilation = false;
+            show_outliers = false;
+
+            for page=this.Pages
+                filename=fullfile(this.PageDir,sprintf(this.PageImgPattern,page));
+                ps = PageScan(filename);
+                ps.show_marked_page_img('Background',bg,...
+                            'ShowDilation',show_dilation,...
+                            'ShowOutliers',show_outliers);
+                title(sprintf('Page %d', page));
+                drawnow;
+                uiwait(gcf);
+            end
         end
 
     end
