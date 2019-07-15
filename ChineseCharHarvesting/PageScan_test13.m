a7604b54a5dff30f06ac265165109ce0e6bc8493
Marek Rychlik <marek@marekrychlik.com>
2019-07-15 12:24:37 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test13.m
+++ ChineseCharHarvesting/PageScan_test13.m
@@ -14,8 +14,8 @@ for page=pages
 
     for i=1:ps.CharacterCount
         BW = ps.Characters(i).CroppedMonoImage;
-        BW = padarray(~BW,[10 10],0,'both');
-        str=r.recognize(BW);
+        BW = padarray(BW,[10 10],0,'both');
+        str=r.recognize(~BW);
         imshow(BW);
         title(str(1));
         pause(2);
