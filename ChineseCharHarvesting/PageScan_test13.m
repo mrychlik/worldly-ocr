48554bccda7b56bd8fce7f6752c4b19474745399
Marek Rychlik <marek@marekrychlik.com>
2019-07-15 12:15:54 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/PageScan_test13.m
@@ -0,0 +1,28 @@
+disp(mfilename);
+
+pagedir='Pages';
+page_img_pattern='page-%02d.ppm';
+if ~exist('pages','var') pages=6:95; end;
+
+r = TesseractRecognizer;
+
+for page=pages
+    filename=fullfile(pagedir,sprintf(page_img_pattern,page));
+    ps = PageScan(filename,'KeepOutliers',keep_outliers);
+    ps = ps.do_merge_characters_all;
+
+    for i=1:ps.CharacterCount
+        BW = ps.Characters(i).CroppedMonoImage;
+        str=r.recognize(BW);
+        imshow(BW);
+        title(str(1));
+        pause(2);
+    end
+    ps.show_ocr;
+    title(sprintf('Page %d', page));
+    drawnow;
+    uiwait(gcf);
+    %pause;
+end;
+
+
