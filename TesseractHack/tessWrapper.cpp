7cc0ed24c6f00a41360415e5c97046ccc576fc98
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 07:17:35 -0700

    *** empty log message ***

--- TesseractHack/tessWrapper.cpp
+++ TesseractHack/tessWrapper.cpp
@@ -38,12 +38,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
 
   if (nrhs < 1 || !mxIsUint8(prhs[0])) mexErrMsgTxt("Must call tessWrapper with the image to OCR.");
-  //	if (nlhs != 1) mexErrMsgTxt("tessWrapper only returns one value: the ocr text");
 
   char lang[16] = "eng";
+
   if (nrhs >= 2) {
-    int len = mxGetM(prhs[1])*mxGetN(prhs[1]);
-    if (!mxIsChar(prhs[1])) mexErrMsgTxt("You must specify the language as a string (typically 3 letters).");
+
+    int len = mxGetM(prhs[1]) * mxGetN(prhs[1]);
+
+    if (!mxIsChar(prhs[1])) {
+      mexErrMsgTxt("You must specify the language as a string (typically 3 letters).");
+    }
 
     if (len > 0) mxGetString(prhs[1],lang,sizeof(lang));
   }
@@ -56,16 +60,22 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
   tesseract::TessBaseAPI ocrApi;
   if (ocrApi.Init(tessbase, lang)) {
+
     mxArray *rhs[1], *lhs[1];
     char *path, *name;
+
     rhs[0] = mxCreateString("fullpath");
     mexCallMATLAB(1, lhs, 1, rhs, "mfilename");
     mxDestroyArray(rhs[0]);
     path = mxArrayToString(lhs[0]);
     mxDestroyArray(lhs[0]);
 
-    if (ocrApi.Init(path, lang)) mexWarnMsgTxt("Can not find language, defaulting to English.");
-    if (ocrApi.Init(path, "eng")) mexErrMsgTxt("error initializing tesseract");
+    if (ocrApi.Init(path, lang)) {
+      mexWarnMsgTxt("Can not find language, defaulting to English.");
+    }
+    if (ocrApi.Init(path, "eng")) {
+      mexErrMsgTxt("error initializing tesseract");
+    }
 
     mxFree(path);
   }
