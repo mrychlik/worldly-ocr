1142292cd296ee6c130ed72107db1e1aa38e7126
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 14:00:37 -0700

    *** empty log message ***

--- ChineseCharHarvesting/runWrapper.m
+++ ChineseCharHarvesting/runWrapper.m
@@ -1,5 +1,5 @@
 [I0,cmap] = imread('Pages/page-06.ppm');
 I = rgb2gray(I0);
 J = uint8(I');
-out = tessWrapper(J,'chi_tra_vert','/usr/local/share/tessdata');
+out = tessWrapper(J,'chi_tra','/usr/local/share/tessdata');
 disp(out);
