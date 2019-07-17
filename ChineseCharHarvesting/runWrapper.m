824551d8f83c7d79fc919d7225ff5a0ccfae207b
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 10:31:44 -0700

    *** empty log message ***

--- ChineseCharHarvesting/runWrapper.m
+++ ChineseCharHarvesting/runWrapper.m
@@ -1,6 +1,8 @@
 [I0,cmap] = imread('Pages/page-06.ppm');
-% Must transpose the image to work? Why.
 I = rgb2gray(I0);
+% We tried 'chi_tra_vert', but it crashes. Therefore,
+% we transposed the image, which allows us to use 'chi_tra'.
+% Too bad...
 J = uint8(I');
 out = tessWrapper(J,'chi_tra');
 disp(out);
