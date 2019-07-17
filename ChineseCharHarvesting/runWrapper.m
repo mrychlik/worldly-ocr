9a07d2b6f0450b88059d43c5bfa1b39dd84cbd25
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 10:29:39 -0700

    *** empty log message ***

--- ChineseCharHarvesting/runWrapper.m
+++ ChineseCharHarvesting/runWrapper.m
@@ -1,6 +1,6 @@
 [I0,cmap] = imread('Pages/page-06.ppm');
 % Must transpose the image to work? Why.
 I = rgb2gray(I0);
-J = uint8(I);
-out = tessWrapper(J,'chi_tra_vert');
+J = uint8(I');
+out = tessWrapper(J,'chi_tra');
 disp(out);
