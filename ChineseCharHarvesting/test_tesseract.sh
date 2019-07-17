fcff04aa42b75be76d6ad1d5f0d26efb25b72d90
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 10:54:25 -0700

    *** empty log message ***

--- ChineseCharHarvesting/test_tesseract.sh
+++ ChineseCharHarvesting/test_tesseract.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # This will crash Tesseract with --oem 2 (i.e. LSTM engine)
+export TESSDATA_PREFIX=/usr/local/share/tessdata
+
 tesseract --oem 2 -l chi_tra_vert Pages/page-06.ppm base
 #tesseract --oem 1 -l chi_tra_vert Pages/page-06.ppm base
