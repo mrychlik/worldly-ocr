40c9bf72e07a38345de9e2ab8affa24f4f7b9c5e
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 08:32:20 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -294,7 +294,7 @@ classdef PageScan < handle
             len = numel(label_str);
             for i = 1:len
                 waitbar(i/len,bh);
-                BW = draw_unicode_char(str(i), 'Helvetica', p.Results.FontSize);
+                BW = draw_unicode_char(label_str{i}, 'Helvetica', p.Results.FontSize);
                 %imagesc(Font.Bitmaps{1}); drawnow; pause(2);
                 I = imresize(BW,[h(i),w(i)]);
                 im = image(x(i),y(i),255*I);
