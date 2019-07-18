8b480b340d6b8a429ccff3f9b1cae4326bc1197d
Marek Rychlik <marek@marekrychlik.com>
2019-07-17 18:07:33 -0700

    *** empty log message ***

--- ChineseCharHarvesting/runWrapperWithConfidence.m
+++ ChineseCharHarvesting/runWrapperWithConfidence.m
@@ -3,5 +3,6 @@ I = rgb2gray(I0);
 J = uint8(fliplr(I));
 ROI = [0,0,1000,100; 
        300,300,200,200];
-out = tessWrapperWithConfidence(J,'chi_tra_vert','/usr/local/share/tessdata',ROI)
+out = tessWrapperWithConfidence(J,'chi_tra_vert','/usr/local/share/tessdata', ...
+                                ROI)
 disp(out);
