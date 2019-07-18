4f58e17c5ce3efcf2a5ae6a7ae6af2d2f20478df
Marek Rychlik <marek@marekrychlik.com>
2019-07-18 15:16:13 -0700

    *** empty log message ***

--- ChineseCharHarvesting/runWrapperWithConfidence.m
+++ ChineseCharHarvesting/runWrapperWithConfidence.m
@@ -11,7 +11,7 @@
 
 mfilename;
 
-[I0,cmap] = imread('Pages/page-06.ppm');
+[I0, cmap] = imread('Pages/page-06.ppm');
 I = rgb2gray(I0);
 J = uint8(fliplr(I));
 % ROI = [0,0,1000,100; 300,300,200,200];
