f30a7e70c1d85eb2a73d420815d0a101d3cfa94a
Marek Rychlik <marek@marekrychlik.com>
2019-07-18 16:16:25 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -1123,7 +1123,7 @@ classdef PageScan < handle
             ignored=[this.Characters.Ignore];
             this.MexOcrResultsCache = struct('Text',[]);
 
-            I = ~this.PageImageMono;
+            I = this.PageImage;
             J = uint8(fliplr(I));
 
             x1 = this.ROI(:,1);
