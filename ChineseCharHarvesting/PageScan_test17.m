f462bf08f18f9dd2ace301ba01b4ce41551898c2
Marek Rychlik <marek@marekrychlik.com>
2019-07-19 08:34:19 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test17.m
+++ ChineseCharHarvesting/PageScan_test17.m
@@ -5,8 +5,8 @@ disp(mfilename);
 config_pages;
 
 keep_outliers=false;
-se = strel('disk',2);
-%se = strel('rectangle',[4,4]);
+%se = strel('disk',2);
+se = strel('rectangle',[4,4]);
 padding = [5 5];
 
 for page=pages
