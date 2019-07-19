6f5d158e13f1544e690554f409fcefb3b444c5ba
Marek Rychlik <marek@marekrychlik.com>
2019-07-18 19:27:49 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -988,7 +988,7 @@ classdef PageScan < handle
                             % very short character, like 'one' (bar)
                             s = c0.Stats;
                             bbox = s.BoundingBox;
-                            d1 = max(d - 5, 0);
+                            d1 = max(d - this.min_vert_gap, 0);
                             bbox1 = [bbox(1), bbox(2) - d1,...
                                      bbox(3),...
                                      bbox(4) + 2*d1
