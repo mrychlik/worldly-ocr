23381f7b7f3f8bf0b95fbc8a60bcbd7bc254ca08
Marek Rychlik <marek@marekrychlik.com>
2019-07-19 00:56:08 -0700

    *** empty log message ***

--- ChineseCharHarvesting/config_pages.m
+++ ChineseCharHarvesting/config_pages.m
@@ -1,5 +1,15 @@
-pagedir='Pages';
-% pagedir='Pages3';
-page_img_pattern='page-%02d.ppm';
-%page_img_pattern='page-%03d.ppm';
+config_num = 1;
+
+switch config_num
+  case 1,
+    pagedir='Pages';
+    page_img_pattern='page-%02d.ppm';
+  case 2,
+    pagedir='Pages2';
+    page_img_pattern='page-%03d.ppm';
+  case 3,
+    pagedir='Pages3';
+    page_img_pattern='page-%02d.ppm';
+end
+
 if ~exist('pages','var') pages=6:95; end;
