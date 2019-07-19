0f927fbcd3fb8a5424bc3f09dad28eddd8e26172
Marek Rychlik <marek@marekrychlik.com>
2019-07-18 18:52:20 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -26,7 +26,7 @@ classdef PageScan < handle
         merge_threshold = 20;           % For attaching "cloud"
         max_char_width = 100;           % Maximum width of a valid character
         min_char_height = 10;           % Minimum height of a valid character
-        bin_threshold = 0.3;            % Binarization threshold
+        bin_threshold = 0.35;           % Binarization threshold
         tesseract_version = 'builtin';% Whether use MATLAB Tesseract, % external or mex
     end
 
