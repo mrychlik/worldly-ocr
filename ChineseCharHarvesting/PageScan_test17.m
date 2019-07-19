24dce69b60e7e0664e1e705c7bd1f736e14ed317
Marek Rychlik <marek@marekrychlik.com>
2019-07-19 08:17:01 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test17.m
+++ ChineseCharHarvesting/PageScan_test17.m
@@ -5,8 +5,8 @@ disp(mfilename);
 config_pages;
 
 keep_outliers=false;
-%se = strel('disk',2);
-se = strel('rectangle',[4,4]);
+se = strel('disk',2);
+%se = strel('rectangle',[4,4]);
 padding = [10 10];
 
 r = TesseractRecognizer('Language','chi_tra','PageSegmentationMode',10);
