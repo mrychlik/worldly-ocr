d9b249eb42c15795a28d0986193809bf5bd95d81
Marek Rychlik <marek@marekrychlik.com>
2019-07-18 17:01:26 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -1138,8 +1138,8 @@ classdef PageScan < handle
             [H, W] = size(I);
 
             % Enlarge boxes
-            dw = 10;
-            dh = 10;
+            dw = 3;
+            dh = 3;
             x1 = max(0, x1 - dw);
             y1 = max(0, y1 - dh);
             x2 = min(W, x2 + dw);
