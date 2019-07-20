d5b0ae1ed9a35f32808012185d9e0e8750b8d747
Marek Rychlik <marek@marekrychlik.com>
2019-07-20 10:46:50 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -140,7 +140,7 @@ classdef PageScan < handle
             this.opts = p.Results;
 
             if isempty(this.opts.FontManager)
-                this.FontManager = FontManager('Font', this.opts.Font, ...
+                this.FontManager = FontManager('FontName', this.opts.Font, ...
                                                'FontSize', this.opts.FontSize);
             else 
                 this.FontManager = this.opts.FontManager;
