75a5cb5fbf9207e4af9237cec556cbbf80d408e8
Marek Rychlik <marek@marekrychlik.com>
2019-07-20 10:21:44 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -1347,26 +1347,6 @@ function D = interval_dist(a, b)
     end
 end
 
-
-function BW = draw_unicode_char(c, Font, FontSize) 
-% DRAW_UNICODE_CHAR - draw a single unicode character
-%   BW = DRAW_UNICODE_CHAR(C, FONT, FONTSIZE) draws
-%   Unicode character C in font FONT, using font size
-%   FONTSIZE in pixels. It returns the generated image
-    fh = figure('Units', 'pixels', 'Color', [1,1,1],'visible','off');
-    ax = axes(fh,'Position',[0 0 1 1],'Units','Normalized','visible','off');
-    axis off;
-    th = text(ax, 0,0,c,'FontSize', FontSize, 'Interpreter','none','Units', ...
-              'pixels','HorizontalAlignment','Left','VerticalAlignment','Bottom');
-    ex = get(th,'Extent');
-    F = getframe(fh);
-    BW = im2bw(F.cdata);
-    [h,~] = size(BW);
-    bbox = round([ex(1)+1,h-ex(4)-1,ex(3),ex(4)]);
-    BW = BW( bbox(2):(bbox(2)+bbox(4)), bbox(1):(bbox(1)+bbox(3)));
-    delete(fh);
-end
-
 function fixedstr = fix_tess_output(str)
 % FIX_TESS_OUTPUT - fixes Tesseract output to be a single character
 %FIXEDSTR = FIX_TESS_OUTPUT(STR)
