860a54baf52f339dadd2ae3591e48377fe52ed53
Marek Rychlik <marek@marekrychlik.com>
2019-07-21 10:10:01 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan.m
+++ ChineseCharHarvesting/PageScan.m
@@ -175,6 +175,7 @@ classdef PageScan < handle
         function Centroids = get.Centroids(this)
             fh = @(s)s.Stats.Centroid';
             Centroids = cell2mat(arrayfun(fh,this.Characters,'UniformOutput',false))';
+            % Set to empty array with 2 columns, to avoid exceptional processing
             if isempty(Centroids)
                 Centroids = zeros(0,2);
             end
