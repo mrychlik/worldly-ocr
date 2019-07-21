f9ee8d7f3a3318b3bb26ef630d150d7a2193af98
Marek Rychlik <marek@marekrychlik.com>
2019-07-21 04:55:51 -0700

    *** empty log message ***

--- ChineseCharHarvesting/config_pages.m
+++ ChineseCharHarvesting/config_pages.m
@@ -17,5 +17,5 @@ if ~exist('pages','var') pages=6:95; end;
 
 % Create a font manager (or use an existing one)
 if ~exist('font_manager')
-    font_manager = FontManagerSQLite;
+    font_manager = FontManagerRAM;
 end
