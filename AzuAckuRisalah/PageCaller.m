6d440b6b0d3088cda5264180a2f0f4627c2a7370
smvazirizade <s.m.vazirizade@gmail.com>
2019-07-21 20:14:35 -0700

    1

---
+++ AzuAckuRisalah/PageCaller.m
@@ -0,0 +1,16 @@
+function [imgname] = PageCaller()
+%You can choose the pages you want to be called here:
+imgname       =["page-08.ppm"];                % many bullet items
+imgname(end+1)=["page-14.ppm"];
+imgname(end+1)=["page-18.ppm"];                % a little diagonal
+imgname(end+1)=["page-23.ppm"];                % numbers in it
+imgname(end+1)=["page-28.ppm"];                % numbers in it
+imgname(end+1)=["page-31.ppm"];                % 
+imgname(end+1)=["page-34.ppm"];
+imgname(end+1)=["page-47.ppm"];
+imgname(end+1)=["page-58.ppm"];
+imgname(end+1)=["page-61.ppm"];                % all text
+% imgname(end+1)=["page-67.ppm"];                % just 4 lines and the rest is white page
+% imgname(end+1)=["page-07.ppm"];                % the resolution is different
+end
+
--- AzuAckuRisalah/script4.m
+++ AzuAckuRisalah/script4.m
@@ -8,17 +8,17 @@
         % Data.mat
 clc;clear
 dirpathsave='imagesamples';     %direction for saving each line
-imgname='page-14.ppm';
+imgname       =PageCaller();     
 enc = 'UTF-8';
 Lang=1;                          %You can chose 
                                                     %1 for Pashto
                                                     %2 for Persian
                                                     %3 for Comparing them
-
-
-for lablel=1:24
+for i=1:length(imgname)                                            
+imgfname = fullfile(dirpathsave,imgname(i),'*.png');
+for lablel=1:numel(dir(imgfname))
 name1=sprintf('image%03.0f.png',lablel);
-imgfname = fullfile(dirpathsave,imgname,name1);
+imgfname = fullfile(dirpathsave,imgname(i),name1);
 BW = imread(imgfname);
 imshow(BW);
 psm = 7;
@@ -48,13 +48,14 @@ switch Lang
 end
 
 name2=sprintf('image%03.0f.txt',lablel);
-fulname = fullfile(dirpathsave,imgname,name2);
+fulname = fullfile(dirpathsave,imgname(i),name2);
 fid=fopen(fulname,'w','n',enc);
 fprintf(fid,'%s',native2unicode(str1,enc));
 fclose('all');
 
-Data.Input{lablel}=BW;
-Data.Output{lablel}=str1;
+Data.Input{i,lablel}=BW;
+Data.Output{i,lablel}=str1;
 end
-fulname = fullfile(dirpathsave,imgname,"Data");
+end
+fulname = fullfile(dirpathsave,"Data");
 save(fulname,"Data")
