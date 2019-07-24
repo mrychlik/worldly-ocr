c3dbdad33a001652e8e4a7009293b59d43743024
Marek Rychlik <marek@marekrychlik.com>
2019-07-24 06:53:22 -0700

    *** empty log message ***

--- ChineseCharHarvesting/FontManagerSQLite_test1.m
+++ ChineseCharHarvesting/FontManagerSQLite_test1.m
@@ -1,9 +1,10 @@
-fm = FontManagerSQLite;
+pfm = FontManagerSQLite;
 
 BW_A = fm.get_char_image('A');
 BW_B = fm.get_char_image('B');
 BW_A_1 = fm.get_char_image('A');
 BW_A_2 = fm.get_char_image('B');
 
+figure;
 subplot(1,2,1),imagesc(BW_A);
 subplot(1,2,2),imagesc(BW_B);
