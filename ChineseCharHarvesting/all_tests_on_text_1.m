646255667537102f47bcaffb03a547a3dba2fb71
Marek Rychlik <marek@marekrychlik.com>
2019-07-27 12:03:22 -0700

    *** empty log message ***

--- ChineseCharHarvesting/all_tests_on_text_1.m
+++ ChineseCharHarvesting/all_tests_on_text_1.m
@@ -2,6 +2,6 @@
 % Note that texts are defined in script config_pages.m
 % Configure sample text 1 and run tests
 
-config_num = 1;
-page = 6:95
+config_num = 1;                         % Pick configuration for text 1
+page = 6:10                             % Override the of number pages of text 1
 PageScan_all_tests
