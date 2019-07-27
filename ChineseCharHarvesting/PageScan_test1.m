fb784c4a864e11919702143124541d956b4311de
Marek Rychlik <marek@marekrychlik.com>
2019-07-27 11:58:12 -0700

    *** empty log message ***

--- ChineseCharHarvesting/PageScan_test1.m
+++ ChineseCharHarvesting/PageScan_test1.m
@@ -7,15 +7,15 @@ config_pages;
 opts_main.KeepOutliers = false;
 
 % opts.Background = 'Mono';
-opts.Background = 'Original';
-opts.ShowDilation = true;
-opts.ShowOutliers = false;
+opts_show.Background = 'Original';
+opts_show.ShowDilation = true;
+opts_show.ShowOutliers = false;
 
 
 for page=pages
     filename=fullfile(pagedir,sprintf(page_img_pattern,page));
     ps = PageScan(filename,opts_main);
-    ps.show_marked_page_img(opts);
+    ps.show_marked_page_img(opts_show);
     title(sprintf('Page %d', page));
     drawnow;
     uiwait(gcf);
