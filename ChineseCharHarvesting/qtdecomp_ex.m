2f633740bc2f78779cfdb2151bb2c21369747f9a
Marek Rychlik <marek@marekrychlik.com>
2019-07-29 10:16:30 -0700

    *** empty log message ***

--- ChineseCharHarvesting/qtdecomp_ex.m
+++ ChineseCharHarvesting/qtdecomp_ex.m
@@ -3,14 +3,16 @@
 %I = imread('liftingbody.png');
 I0 = imread('Pages3/page-06.ppm');
 I1 = rgb2gray(I0);
+I2 = im2bw(I0);
+
 sz = size(I1);
 log2_sz = ceil(log2(sz));
 I=padarray(I1,2.^log2_sz-sz,0,'post');
 
 thresh = 0.5;
-S = qtdecomp(I,thresh,16);
+S = qtdecomp(I,thresh);
 blocks = repmat(uint8(0),size(S));
-for dim = [16 8 4 2 1];    
+for dim = [32,16 8 4 2 1];    
     numblocks = length(find(S==dim));    
     if (numblocks > 0)        
         values = repmat(uint8(1),[dim dim numblocks]);
@@ -20,17 +22,21 @@ for dim = [16 8 4 2 1];
 end
 blocks(end,1:end) = 1;
 blocks(1:end,end) = 1;
-imshow(I),
-
-figure,
 hold on;
 
+subplot(1,2,1);
+I=I(1:sz(1),1:sz(2));
+imshow(I),
+
+
+
+subplot(1,2,2);
 blocks = blocks(1:sz(1),1:sz(2));
-im1 = image(blocks);
+im1 = imshow(blocks,[]);
 im1.AlphaData = 0.5;
 
-I=I(1:sz(1),1:sz(2));
-im2 = image(I);
-im2.AlphaData = 0.5;
+I2=imdilate(I2,strel('rectangle',[2,2]));
+im2 = imshow(I2);
+
 
 hold off;
