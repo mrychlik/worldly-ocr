c9528d8db64246bac0228c89d264cb93a286804f
Marek Rychlik <marek@marekrychlik.com>
2019-07-29 15:41:03 -0700

    *** empty log message ***

---
+++ ChineseCharHarvesting/qtdecomp_ex2.m
@@ -0,0 +1,46 @@
+%%    View the block representation of quadtree decomposition.
+
+%I = imread('liftingbody.png');
+I0 = imread('Pages3/page-06.ppm');
+I1 = rgb2gray(I0);                      % Must make an intensity image
+I2 = im2bw(I0);
+
+sz = size(I1);
+log2_sz = ceil(log2(sz));
+I = padarray(I1,2.^log2_sz-sz,0,'post');
+
+S = qtdecomp(I,@thresh);
+blocks = repmat(uint8(0),size(S));
+
+for dim = [256,128,64,32,16 8 4];    
+    %c = 10*ceil(log2(dim));              % color
+    c=uint8(255);
+    numblocks = length(find(S==dim));    
+    if (numblocks > 0)        
+        values = repmat(uint8(255),[dim dim numblocks]);
+        values(2:dim,2:dim,:) = c;
+        blocks = qtsetblk(blocks,S,dim,values);
+    end
+end
+
+blocks(end,1:end) = 1;
+blocks(1:end,end) = 1;
+
+
+ax1=subplot(1,3,1);
+I = I(1:sz(1),1:sz(2));
+imshow(I,[]),
+
+ax2=subplot(1,3,[2,3]);
+blocks = blocks(1:sz(1),1:sz(2));
+imshow(blocks,[]);
+
+linkaxes([ax1,ax2]);
+
+
+function rv = thresh(B)
+    if max(B(:)) < 100
+        rv = false;
+    end
+    rv = true;
+end
