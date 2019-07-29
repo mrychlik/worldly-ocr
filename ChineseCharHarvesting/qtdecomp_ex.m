503a6e33e0a5834110e3a413977201b19f29f29a
Marek Rychlik <marek@marekrychlik.com>
2019-07-29 15:20:31 -0700

    *** empty log message ***

--- ChineseCharHarvesting/qtdecomp_ex.m
+++ ChineseCharHarvesting/qtdecomp_ex.m
@@ -9,15 +9,17 @@ sz = size(I1);
 log2_sz = ceil(log2(sz));
 I = padarray(I1,2.^log2_sz-sz,0,'post');
 
-thresh = 128;
+thresh = 0.28;
 S = qtdecomp(I,thresh);
 blocks = repmat(uint8(0),size(S));
 
-for dim = [32,16 8 4 2 1];    
+for dim = [256,128,64,32,16 8 4];    
+    %c = 10*ceil(log2(dim));              % color
+    c=uint8(255);
     numblocks = length(find(S==dim));    
     if (numblocks > 0)        
-        values = repmat(uint8(1),[dim dim numblocks]);
-        values(2:dim,2:dim,:) = 0;
+        values = repmat(c,[dim dim numblocks]);
+        %values(2:dim,2:dim,:) = c;
         blocks = qtsetblk(blocks,S,dim,values);
     end
 end
@@ -26,13 +28,13 @@ blocks(end,1:end) = 1;
 blocks(1:end,end) = 1;
 hold on;
 
-subplot(1,2,1);
+subplot(1,3,1);
 I = I(1:sz(1),1:sz(2));
 imshow(I),
 
 
 
-subplot(1,2,2);
+subplot(1,3,[2,3]);
 blocks = blocks(1:sz(1),1:sz(2));
 im1 = imshow(blocks,[]);
 %im1.AlphaData = 0.5;
