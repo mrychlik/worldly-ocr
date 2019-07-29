4a5cccfe29b62b23970cef3fdb3a87193bdcb0a8
Marek Rychlik <marek@marekrychlik.com>
2019-07-29 12:14:56 -0700

    *** empty log message ***

--- ChineseCharHarvesting/qtdecomp_ex.m
+++ ChineseCharHarvesting/qtdecomp_ex.m
@@ -2,14 +2,14 @@
 
 %I = imread('liftingbody.png');
 I0 = imread('Pages3/page-06.ppm');
-I1 = rgb2gray(I0);
+I1 = rgb2gray(I0);                      % Must make an intensity image
 I2 = im2bw(I0);
 
 sz = size(I1);
 log2_sz = ceil(log2(sz));
 I=padarray(I1,2.^log2_sz-sz,0,'post');
 
-thresh = 0.5;
+thresh = 128;
 S = qtdecomp(I,thresh);
 blocks = repmat(uint8(0),size(S));
 for dim = [32,16 8 4 2 1];    
@@ -25,7 +25,7 @@ blocks(1:end,end) = 1;
 hold on;
 
 subplot(1,2,1);
-I=I(1:sz(1),1:sz(2));
+I = I(1:sz(1),1:sz(2));
 imshow(I),
 
 
@@ -33,10 +33,10 @@ imshow(I),
 subplot(1,2,2);
 blocks = blocks(1:sz(1),1:sz(2));
 im1 = imshow(blocks,[]);
-im1.AlphaData = 0.5;
+%im1.AlphaData = 0.5;
 
-I2=imdilate(I2,strel('rectangle',[2,2]));
-im2 = imshow(I2);
+%I2=imdilate(I2,strel('rectangle',[2,2]));
+%im2 = imshow(I2);
 
 
 hold off;
