40d31d4c6c2f1b7f36ad0bde2e647ffd46c728dc
Marek Rychlik <marek@marekrychlik.com>
2019-07-30 10:48:47 -0700

    *** empty log message ***

--- ChineseCharHarvesting/char_img_database.m
+++ ChineseCharHarvesting/char_img_database.m
@@ -19,44 +19,47 @@ exec(conn, [ 'create table if not exists char_bitmaps ' ...
              'image VARCHAR)' ]);
 
 
-for page=pages
-    disp(sprintf('Page: %d', page));
-    found_first_char = false;
-    for idx=1:max_char
-        filename = fullfile(char_dir,sprintf(char_img_pattern,page, ...
-                                             idx));
-        if exist(filename, 'file') == 2
-            if ~found_first_char
-                disp(sprintf('Found first char %d on page %d', idx, page));                
-                found_first_char = true;
+try
+    for page=pages
+        disp(sprintf('Page: %d', page));
+        found_first_char = false;
+        for idx=1:max_char
+            filename = fullfile(char_dir,sprintf(char_img_pattern,page, ...
+                                                 idx));
+            if exist(filename, 'file') == 2
+                if ~found_first_char
+                    disp(sprintf('Found first char %d on page %d', idx, page));                
+                    found_first_char = true;
+                end
+            else
+                if found_first_char
+                    disp(sprintf('Reached last char %d on page %d', idx, ...
+                                 page));
+                end
+                break;
             end
-        else
             if found_first_char
-                disp(sprintf('Reached last char %d on page %d', idx, ...
-                             page));
+                disp(sprintf('\tChar: %d\n', idx));        
             end
-            break;
-        end
-        if found_first_char
-            disp(sprintf('\tChar: %d\n', idx));        
-        end
-    
-        if ~found_first_char
-            continue;
-        end
-        I = imread(filename);
-        imshow(I);
-        title(sprintf('Page %d, char %d', page, idx));
-        drawnow; pause(0.2);
+            
+            if ~found_first_char
+                continue;
+            end
+            I = imread(filename);
+            imshow(I);
+            title(sprintf('Page %d, char %d', page, idx));
+            drawnow; pause(0.2);
 
-        % Write to database
-        BW = im2bw(I);
-        BW = imautocrop(BW);
-        BW_data = pack_binary_image(BW);
-        insert(conn, 'bitmaps',...
-               {'page', 'idx', 'image'},...
-               {page, idx, BW_data} );
+            % Write to database
+            BW = im2bw(I);
+            BW = imautocrop(BW);
+            BW_data = pack_binary_image(BW);
+            insert(conn, 'char_bitmaps',...
+                   {'page', 'idx', 'image'},...
+                   {page, idx, BW_data} );
+        end
     end
+catch me
+    disp(me);
+    close(conn);
 end
-
-close(conn);
