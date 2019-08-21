ddd2a8390f754280f3b13f41f8a2e48968db2ebe
Marek Rychlik <marek@marekrychlik.com>
2019-08-21 11:24:34 -0700

    *** empty log message ***

---
+++ LogisticRegression/my_script_w_regularizer.m
@@ -0,0 +1,32 @@
+% This script tests the new regularization method
+
+digit0 = 0;
+digit1 = 1;
+digit2 = 2;
+digit3 = 3;
+[X,T] = prepare_training_data(digit0,digit1,digit2,digit3);
+% Matlab expects samples in columns and T to be a row vector
+
+
+% Insert a row of 1's
+X = [ones([1,size(X,2)]);X];
+
+% Regularize T
+[D,N] = size(X);
+[C,~]=size(T);
+
+t=.999;                % 1-t is the probability of assigning class at random
+                       %t=1;                                    % Don't regularize
+                                        
+if t<1 
+    X=[X,-X];
+    T=[t*T,(1-t)*T];
+end
+
+% Straight from PATTERNNET help page
+num_epochs = 5000;
+[Y, NErrors] = train_patternnet(X,T,num_epochs);
+
+figure;
+plotconfusion(T,Y);
+NErrors
