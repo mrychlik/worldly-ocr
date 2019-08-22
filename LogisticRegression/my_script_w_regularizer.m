380169bbc330285f68802f4bddaf23f4c4144a76
Marek Rychlik <marek@marekrychlik.com>
2019-08-21 22:28:46 -0700

    *** empty log message ***

--- LogisticRegression/my_script_w_regularizer.m
+++ LogisticRegression/my_script_w_regularizer.m
@@ -7,7 +7,6 @@ digit3 = 3;
 [X,T] = prepare_training_data(digit0,digit1,digit2,digit3);
 % Matlab expects samples in columns and T to be a row vector
 
-
 % Straight from PATTERNNET help page
 num_epochs = 5000;
 [Y, NErrors,W] = train_patternnet_w_regularizer(X,T,num_epochs);
