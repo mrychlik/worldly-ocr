1ec0e0920ffc783e8a4a9b95c2db1f31d01f8c31
Marek Rychlik <marek@marekrychlik.com>
2019-08-24 10:37:07 -0700

    *** empty log message ***

--- LogisticRegression/my_script_self_regularizer.m
+++ LogisticRegression/my_script_self_regularizer.m
@@ -11,13 +11,18 @@ digit3 = 3;
 [D,N] = size(X);
 [C,~]=size(T);
 
+% Add regularizing sample
 gamma=1;
-X = [X,gamma*eye(D)];
-T = [T,1/C*ones(C,D)];
+X1 = [X,gamma*eye(D)];
+T1 = [T,1/C*ones(C,D)];
 
 % Straight from PATTERNNET help page
 num_epochs = 5000;
-[Y, NErrors,W] = train_patternnet_no_regularizer(X,T,num_epochs);
+[Y1, NErrors,W] = train_patternnet_no_regularizer(X1,T1,num_epochs);
+
+% Drop part due to regularization
+Y=Y1(:,1:N);
+NErrors = length(find(round(Y)~=round(T)));
 
 figure;
 plotconfusion(T,Y);
