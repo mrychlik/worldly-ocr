1df1698d222cd516be92fffd52d474addaeb0f96
Marek Rychlik <marek@marekrychlik.com>
2020-04-08 21:45:27 -0700

    *** empty log message ***

--- ChineseFineArts/script.m
+++ ChineseFineArts/script.m
@@ -1,4 +1,4 @@
 % Parameters to decode a page of this newspaper 
 filepath=fullfile('..','Data','38059-000001.png');
 I=imread(filepath);
-BW = binarize(I,'adaptive','ForegroundPolarity','dark','Seinsitivity',0.8);
+BW = imbinarize(I,'adaptive','ForegroundPolarity','dark','Sensitivity',0.5);
