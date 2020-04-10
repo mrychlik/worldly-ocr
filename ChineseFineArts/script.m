99cd34075774e5ebff39795e9b6b01172fe578a4
Marek Rychlik <marek@marekrychlik.com>
2020-04-10 14:26:28 -0700

    Created 150DPI resolution of file 38059.pdf

--- ChineseFineArts/script.m
+++
@@ -1,4 +0,0 @@
-% Parameters to decode a page of this newspaper 
-filepath=fullfile('..','Data','38059-000001.png');
-I=imread(filepath);
-BW = imbinarize(I,'adaptive','ForegroundPolarity','dark','Sensitivity',0.5);
